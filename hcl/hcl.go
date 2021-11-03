a828eae4bdee8e7d126e76fe81fbfea3dc58463f
Tiago Natel <t.nateldemoura@gmail.com>
2021-11-03 12:51:12 +0000

    add module source reference doc

--- hcl/hcl.go
+++ hcl/hcl.go
@@ -13,5 +13,6 @@ type ModuleParser interface {
 
 // IsLocal tells if module source is a local directory.
 func (m Module) IsLocal() bool {
+	// As specified here: https://www.terraform.io/docs/language/modules/sources.html#local-paths
 	return m.Source[0:2] == "./" || m.Source[0:3] == "../"
 }
