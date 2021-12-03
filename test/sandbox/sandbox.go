f1089c1f3462b24123a88eece3715271a919e2c7
Tiago Natel <t.nateldemoura@gmail.com>
2021-12-03 16:04:00 +0000

    improve docs

--- test/sandbox/sandbox.go
+++ test/sandbox/sandbox.go
@@ -1,6 +1,5 @@
-// Package sandbox provides an easy way to setup
-// isolated terrastack projects that can be used
-// on testing, acting like sandboxes.
+// Package sandbox provides an easy way to setup isolated terrastack projects
+// that can be used on testing, acting like sandboxes.
 //
 // It helps with:
 //
@@ -21,16 +20,16 @@ import (
 	"github.com/mineiros-io/terrastack/test"
 )
 
-// S is a full sandbox with its own base dir that is an initialized
-// git repo for test purposes.
+// S is a full sandbox with its own base dir that is an initialized git repo for
+// test purposes.
 type S struct {
 	t       *testing.T
 	git     Git
 	basedir string
 }
 
-// DirEntry represents a directory and can be
-// used to create files inside the directory
+// DirEntry represents a directory and can be used to create files inside the
+// directory.
 type DirEntry struct {
 	t       *testing.T
 	abspath string
@@ -43,12 +42,10 @@ type StackEntry struct {
 	DirEntry
 }
 
-// FileEntry represents a file and can be used
-// to manipulate the file contents.
-// It is optimized for reading/writing all contents,
-// not stream programming (io.Reader/io.Writer).
-// It has limited usefulness but it is easier to
-// work with for testing.
+// FileEntry represents a file and can be used to manipulate the file contents.
+// It is optimized for reading/writing all contents, not stream programming
+// (io.Reader/io.Writer).
+// It has limited usefulness but it is easier to work with for testing.
 type FileEntry struct {
 	t    *testing.T
 	path string
@@ -56,10 +53,9 @@ type FileEntry struct {
 
 // New creates a new test sandbox.
 //
-// It is a programming error to use a test env created
-// with a *testing.T other than the one of the test
-// using the test env, for a new test/sub-test always create
-// a new test env for it.
+// It is a programming error to use a test env created with a *testing.T other
+// than the one of the test using the test env, for a new test/sub-test always
+// create a new test env for it.
 func New(t *testing.T) S {
 	t.Helper()
 
@@ -109,25 +105,23 @@ func (s S) BuildTree(layout []string) {
 	}
 }
 
-// Git returns a git wrapper that is useful to run git commands
-// safely inside the test env repo.
+// Git returns a git wrapper that is useful to run git commands safely inside
+// the test env repo.
 func (s S) Git() Git {
 	return s.git
 }
 
-// BaseDir returns the base dir of the test env.
-// All dirs/files created through the test env will
-// be included inside this dir.
+// BaseDir returns the base dir of the test env. All dirs/files created through
+// the test env will be included inside this dir.
 //
-// It is a programming error to delete this dir, it will
-// be automatically removed when the test finishes.
+// It is a programming error to delete this dir, it will be automatically
+// removed when the test finishes.
 func (s S) BaseDir() string {
 	return s.basedir
 }
 
-// CreateModule will create a module dir with the given relative
-// path, returning a directory entry that can be used to create
-// files inside the module dir.
+// CreateModule will create a module dir with the given relative path, returning
+// a directory entry that can be used to create files inside the module dir.
 func (s S) CreateModule(relpath string) DirEntry {
 	t := s.t
 	t.Helper()
@@ -139,9 +133,8 @@ func (s S) CreateModule(relpath string) DirEntry {
 	return newDirEntry(s.t, s.basedir, relpath)
 }
 
-// CreateStack will create a stack dir with the given relative
-// path, returning a stack entry that can be used to create
-// files inside the stack dir.
+// CreateStack will create a stack dir with the given relative path, returning a
+// stack entry that can be used to create files inside the stack dir.
 func (s S) CreateStack(relpath string) *StackEntry {
 	t := s.t
 	t.Helper()
@@ -150,9 +143,6 @@ func (s S) CreateStack(relpath string) *StackEntry {
 		t.Fatalf("CreateStack() needs a relative path but given %q", relpath)
 	}
 
-	// Given the current design assuming ../../modules is safe
-	// But we could change this in the future and maintain the
-	// current API working.
 	stack := &StackEntry{
 		DirEntry: newDirEntry(t, s.basedir, relpath),
 	}
@@ -161,17 +151,16 @@ func (s S) CreateStack(relpath string) *StackEntry {
 	return stack
 }
 
-// CreateFile will create a file inside this dir entry with the
-// given name and the given body. The body can be plain text
-// or a format string identical to what is defined on Go fmt package.
+// CreateFile will create a file inside this dir entry with the given name and
+// the given body. The body can be plain text or a format string identical to
+// what is defined on Go fmt package.
 //
-// It returns a file entry that can be used to further
-// manipulate the created file, like replacing its contents.
-// The file entry is optimized for always replacing the
-// file contents, not streaming (using file as io.Writer).
+// It returns a file entry that can be used to further manipulate the created
+// file, like replacing its contents. The file entry is optimized for always
+// replacing the file contents, not streaming (using file as io.Writer).
 //
-// If the file already exists its contents will be truncated,
-// like os.Create behavior: https://pkg.go.dev/os#Create
+// If the file already exists its contents will be truncated, like os.Create
+// behavior: https://pkg.go.dev/os#Create
 func (de DirEntry) CreateFile(name, body string, args ...interface{}) *FileEntry {
 	de.t.Helper()
 
@@ -195,8 +184,8 @@ func (de DirEntry) RelPath() string {
 }
 
 // Write writes the given text body on the file, replacing its contents.
-// The body can be plain text or a format string identical to what
-// is defined on Go fmt package.
+// The body can be plain text or a format string identical to what is defined on
+// Go fmt package.
 //
 // It behaves like os.WriteFile: https://pkg.go.dev/os#WriteFile
 func (fe FileEntry) Write(body string, args ...interface{}) {
@@ -214,10 +203,9 @@ func (fe FileEntry) Path() string {
 	return fe.path
 }
 
-// ModSource returns the relative import path for the
-// module with the given module dir entry. The path is
-// relative to stack dir itself (hence suitable to be
-// a module source path).
+// ModSource returns the relative import path for the module with the given
+// module dir entry. The path is relative to stack dir itself (hence suitable to
+// be a module source path).
 func (se StackEntry) ModSource(mod DirEntry) string {
 	relpath, err := filepath.Rel(se.abspath, mod.abspath)
 	assert.NoError(se.t, err)
@@ -229,9 +217,8 @@ func (se StackEntry) Path() string {
 	return se.DirEntry.abspath
 }
 
-// RelPath returns the relative path of the stack.
-// It is relative to the base dir of the test environment
-// that created this stack.
+// RelPath returns the relative path of the stack. It is relative to the base
+// dir of the test environment that created this stack.
 func (se StackEntry) RelPath() string {
 	return se.DirEntry.relpath
 }
