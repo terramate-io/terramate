65b04910a841ea3e1a61da8ab08663d07309fbda
Tiago de Bem Natel de Moura <t.nateldemoura@gmail.com>
2021-12-07 12:11:59 +0000

    refactor: remove parser interfaces (#61)

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/alecthomas/kong v0.2.17
 	github.com/google/go-cmp v0.3.1
+	github.com/hashicorp/go-version v1.3.0
 	github.com/hashicorp/hcl/v2 v2.10.1
 	github.com/madlambda/spells v0.2.0
 	github.com/zclconf/go-cty v1.8.0
@@ -13,7 +14,6 @@ require (
 require (
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
-	github.com/hashicorp/go-version v1.3.0 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/text v0.3.5 // indirect
--- hcl/hcl.go
+++ hcl/hcl.go
@@ -1,5 +1,14 @@
 package hcl
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/zclconf/go-cty/cty"
+)
+
 // Module represents a terraform module.
 // Note that only the fields relevant for terrastack are declared here.
 type Module struct {
@@ -11,14 +20,129 @@ type Terrastack struct {
 	RequiredVersion string
 }
 
-// Parser is an interface for terrastack parsers.
-type Parser interface {
-	Parse(path string) (Terrastack, error)
+// Parser is a terrastack parser.
+type Parser struct {
+	p *hclparse.Parser
 }
 
-// ModuleParser is an interface for parsing just the modules from HCL files.
-type ModuleParser interface {
-	ParseModules(path string) ([]Module, error)
+// NewParser creates a HCL parser
+func NewParser() *Parser {
+	return &Parser{
+		p: hclparse.NewParser(),
+	}
+}
+
+// ParseModules parses blocks of type "module" containing a single label.
+func (p *Parser) ParseModules(path string) ([]Module, error) {
+	_, err := os.Stat(path)
+	if err != nil {
+		return nil, fmt.Errorf("stat failed on %q: %w", path, err)
+	}
+
+	f, diags := p.p.ParseHCLFile(path)
+	if diags.HasErrors() {
+		return nil, fmt.Errorf("parsing modules: %w", diags)
+	}
+
+	body, _ := f.Body.(*hclsyntax.Body)
+
+	var modules []Module
+	for _, block := range body.Blocks {
+		if block.Type != "module" || len(block.Labels) != 1 {
+			continue
+		}
+
+		moduleName := block.Labels[0]
+		source, ok, err := findStringAttr(block, "source")
+		if err != nil {
+			return nil, fmt.Errorf("looking for %q.source attribute: %w",
+				moduleName, err)
+		}
+		if !ok {
+			continue
+		}
+
+		modules = append(modules, Module{Source: source})
+	}
+
+	return modules, nil
+}
+
+// Parse parses a terrastack source.
+func (p *Parser) Parse(fname string, data []byte) (*Terrastack, error) {
+	f, diags := p.p.ParseHCL(data, fname)
+	if diags.HasErrors() {
+		return nil, fmt.Errorf("parsing terrastack: %w", diags)
+	}
+
+	body, _ := f.Body.(*hclsyntax.Body)
+
+	var terrastack []*Terrastack
+	for _, block := range body.Blocks {
+		if block.Type != "terrastack" {
+			continue
+		}
+
+		if len(block.Labels) > 0 {
+			return nil, fmt.Errorf("terrastack block must have no labels")
+		}
+
+		reqversion, ok, err := findStringAttr(block, "required_version")
+		if err != nil {
+			return nil, fmt.Errorf("looking for terrastack.required_version attribute: %w",
+				err)
+		}
+		if !ok {
+			return nil, fmt.Errorf("terrastack block requires a \"required_version\" attribute")
+		}
+
+		terrastack = append(terrastack, &Terrastack{
+			RequiredVersion: reqversion,
+		})
+	}
+
+	if len(terrastack) > 1 {
+		return nil, fmt.Errorf("only 1 \"terrastack\" block is allowed but found %d",
+			len(terrastack))
+	}
+
+	if len(terrastack) == 0 {
+		return nil, fmt.Errorf("no \"terrastack\" block found")
+	}
+
+	return terrastack[0], nil
+}
+
+// ParseFile parses a terrastack file.
+func (p *Parser) ParseFile(path string) (*Terrastack, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %q: %w", path, err)
+	}
+
+	return p.Parse(path, data)
+}
+
+func findStringAttr(block *hclsyntax.Block, attr string) (string, bool, error) {
+	for name, value := range block.Body.Attributes {
+		if name != attr {
+			continue
+		}
+
+		attrVal, diags := value.Expr.Value(nil)
+		if diags.HasErrors() {
+			return "", false, fmt.Errorf("failed to evaluate %q attribute: %w",
+				attr, diags)
+		}
+
+		if attrVal.Type() != cty.String {
+			return "", false, fmt.Errorf("attribute %q is not a string", attr)
+		}
+
+		return attrVal.AsString(), true, nil
+	}
+
+	return "", false, nil
 }
 
 // IsLocal tells if module source is a local directory.
