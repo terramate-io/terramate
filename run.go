848f894b804c22866af712fe562a61e9504fe870
Tiago Natel <t.nateldemoura@gmail.com>
2021-12-13 12:07:11 +0000

    fix exec reuse

--- run.go
+++ run.go
@@ -23,20 +23,21 @@ import (
 
 const ErrRunCycleDetected errutil.Error = "cycle detected in run order"
 
-func Run(stacks []Stack, cmd *exec.Cmd) error {
+func Run(stacks []Stack, cmdSpec *exec.Cmd) error {
 	order, err := RunOrder(stacks)
 	if err != nil {
 		return err
 	}
 
 	for _, stack := range order {
-		fmt.Fprintf(cmd.Stdout, "[%s] running %s\n", stack.Dir, cmd)
+		cmd := *cmdSpec
+
+		fmt.Fprintf(cmd.Stdout, "[%s] running %s\n", stack.Dir, &cmd)
 		cmd.Dir = stack.Dir
 		err := cmd.Run()
 		if err != nil {
 			return err
 		}
-
 	}
 
 	return nil
