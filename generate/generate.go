c9d5026e98de68d748cd75b73ae6168e0a3487b9
Tiago Katcipis <tiagokatcipis@gmail.com>
2021-12-31 20:10:38 +0100

    refactor: remove unnecessary check

--- generate/generate.go
+++ generate/generate.go
@@ -4,8 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -157,12 +156,7 @@ func generateStackLocals(
 		localsBody.SetAttributeValue(name, localsAttrs[name])
 	}
 
-	tfcode := gen.Bytes()
-	if len(tfcode) == 0 {
-		return nil
-	}
-
-	tfcode = addHeader(tfcode)
+	tfcode := addHeader(gen.Bytes())
 	genfile := filepath.Join(stackpath, LocalsFilename)
 	return os.WriteFile(genfile, tfcode, 0666)
 }
