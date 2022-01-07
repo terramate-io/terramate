b3122a1c17f61d46676c634cef3af795bee87786
Tiago Cesar Katcipis <tiagokatcipis@gmail.com>
2022-01-07 18:49:36 +0100

    chore: fix docs

--- cmd/terramate/cli/cli_runner_test.go
+++ cmd/terramate/cli/cli_runner_test.go
@@ -71,7 +71,7 @@ func (ts tscli) run(args ...string) runResult {
 		allargs = append(allargs, "--chdir", ts.chdir)
 	}
 	if len(args) > 0 {
-		// Avoid failing test when calling just terramate with no params
+		// Avoid failing test when calling just terramate with no args
 		allargs = append(allargs, "--log-level", "fatal")
 	}
 	allargs = append(allargs, args...)
