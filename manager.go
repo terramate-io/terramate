21c540145df681db48d3887efc70192232e334cd
Tiago de Bem Natel de Moura <t.nateldemoura@gmail.com>
2022-01-21 14:15:17 +0000

    Update manager.go
    Co-authored-by: Tiago Cesar Katcipis <tiagokatcipis@gmail.com>

--- manager.go
+++ manager.go
@@ -402,7 +402,7 @@ func (m *Manager) AddWantedOf(stacks []stack.S) ([]stack.S, error) {
 		for _, s := range wantedBy {
 			ws, err := m.stackLoader.LoadAll(m.root, s.AbsPath(), s.Wants()...)
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("calculating wanted stacks: %v", err)
 			}
 
 			for _, wws := range ws {
