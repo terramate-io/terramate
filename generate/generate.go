75aad442c1780da4f7c6d49c2eede12acff72b24
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-02-08 16:09:24 +0100

    chore: improve error msg

--- generate/generate.go
+++ generate/generate.go
@@ -748,7 +748,7 @@ func checkGeneratedFilesConflicts(genfiles []genfile) error {
 		if _, ok := observed[genf.name]; ok {
 			// TODO(katcipis): improve error with origin info
 			// Right now it is not as nice/easy as I would like :-(.
-			return fmt.Errorf("two configurations produce same file %s", genf.name)
+			return fmt.Errorf("two configurations produce same file %q", genf.name)
 		}
 		observed[genf.name] = struct{}{}
 	}
