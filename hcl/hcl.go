11875ba88d21f893a33dddf643009d7497a7a81d
Tiago Cesar Katcipis <tiagokatcipis@gmail.com>
2022-02-08 16:43:32 +0100

    Update hcl/hcl.go
    Co-authored-by: Nathan Thiesen <me@iloveyounathan.club>

--- hcl/hcl.go
+++ hcl/hcl.go
@@ -897,7 +897,7 @@ func parseGenerateConfig(block *hclsyntax.Block) (GenerateConfig, error) {
 
 	if cfg.LocalsFilename != "" && cfg.LocalsFilename == cfg.BackendCfgFilename {
 		return GenerateConfig{}, fmt.Errorf(
-			"terramate.config.generate: locals and backend cfg filenames have the same %q value", cfg.LocalsFilename)
+			"terramate.config.generate: locals and backend cfg files have the same name %q", cfg.LocalsFilename)
 	}
 
 	return cfg, nil
