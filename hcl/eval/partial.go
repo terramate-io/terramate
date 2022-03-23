e9f5ec21dd2465641c572c8ef97895a27bd0dfbe
Tiago Natel <t.nateldemoura@gmail.com>
2022-03-23 03:34:04 +0000

    refactor

--- hcl/eval/partial.go
+++ hcl/eval/partial.go
@@ -67,12 +67,26 @@ type node struct {
 }
 
 type engine struct {
-	tokens    hclwrite.Tokens
-	pos       int
-	ctx       *Context
-	evaluated []*node
+	tokens hclwrite.Tokens
+	pos    int
+	ctx    *Context
+
+	// evalstack is a stack of evaluated nodes.
+	// The engine walks through the token list evaluating them as needed into a
+	// separated node struct placed in this stack.
+	evalstack []*node
 
 	nparen int
+}
+
+// addsrc adds source token.
+func (n *node) addsrc(toks ...*hclwrite.Token) {
+	n.source = append(n.source, toks...)
+}
+
+// add evaluated token.
+func (n *node) add(toks ...*hclwrite.Token) {
+	n.evaluated = append(n.evaluated, toks...)
 }
 
 func newPartialEngine(tokens hclwrite.Tokens, ctx *Context) *engine {
@@ -96,11 +110,11 @@ func (e *engine) PartialEval() (hclwrite.Tokens, error) {
 		e.commit()
 	}
 
-	if len(e.evaluated) != 1 {
+	if len(e.evalstack) != 1 {
 		panic("invalid number of scratch spaces")
 	}
 
-	return e.evaluated[0].evaluated, nil
+	return e.evalstack[0].evaluated, nil
 }
 
 func (e *engine) hasTokens() bool {
@@ -117,7 +131,7 @@ func (e *engine) peekn(n int) *hclwrite.Token {
 
 func (e *engine) newnode() (int, *node) {
 	n := &node{}
-	e.evaluated = append(e.evaluated, n)
+	e.evalstack = append(e.evalstack, n)
 	return e.tailpos(), n
 }
 
@@ -129,13 +143,13 @@ func (e *engine) commit() {
 	tail := e.tail()
 
 	mergeat := e.tailpos() - 1
-	merge := e.evaluated[mergeat]
-	merge.evaluated = append(merge.evaluated, tail.evaluated...)
-	merge.source = append(merge.source, tail.source...)
-	e.evaluated = e.evaluated[e.headpos() : mergeat+1]
+	merge := e.evalstack[mergeat]
+	merge.add(tail.evaluated...)
+	merge.addsrc(tail.source...)
+	e.evalstack = e.evalstack[e.headpos() : mergeat+1]
 }
 
-func (e *engine) tail() *node { return e.evaluated[e.tailpos()] }
+func (e *engine) tail() *node { return e.evalstack[e.tailpos()] }
 
 func (e *engine) emit() {
 	tail := e.tail()
@@ -275,7 +289,9 @@ loop:
 			closeToken = hclsyntax.TokenCBrack
 		}
 
-		next := e.peekn(1)
+		pos := e.skipNewLines(1)
+
+		next := e.peekn(pos)
 		switch {
 		case isForExpr(next):
 			err = e.evalForExpr(openToken, closeToken)
@@ -612,6 +628,7 @@ func (e *engine) evalForExpr(matchOpenType, matchCloseType hclsyntax.TokenType) 
 	}
 
 	e.emit()
+	e.emitnl()
 
 	// { for
 	tok = e.peek()
@@ -908,12 +925,12 @@ func (e *engine) evalInterp() error {
 		rewritten = append(rewritten, interpClose)
 	}
 
-	e.evaluated[e.tailpos()].evaluated = rewritten
+	e.evalstack[e.tailpos()].evaluated = rewritten
 	return nil
 }
 
 func (e *engine) evalString() error {
-	scratchPos, _ := e.newnode()
+	nodePos, _ := e.newnode()
 	tok := e.peek()
 	if tok.Type != hclsyntax.TokenOQuote {
 		return errorf("bug: not a quoted string")
@@ -946,83 +963,111 @@ func (e *engine) evalString() error {
 	}
 
 	e.pos++
-	rewritten := hclwrite.Tokens{
-		tokenOQuote(),
-	}
 
-	// handles the case of `"${a.b}"`.
-	if e.tailpos()-scratchPos == 1 {
+	rewritten := &node{}
+	rewritten.addsrc(tokenOQuote())
+	rewritten.add(tokenOQuote())
+
+	// handles the case of a single interpolated object, examples:
+	// - "${a.b}"
+	// - "${a}"
+	// - "${0}"
+	// - "${global.something}"
+	if e.tailpos()-nodePos == 1 {
 		e.commit()
 		tail := e.tail()
 		switch tail.evaluated[0].Type {
 		case hclsyntax.TokenQuotedLit, hclsyntax.TokenTemplateInterp:
-			rewritten = append(rewritten, e.tail().evaluated...)
-			rewritten = append(rewritten, tokenCQuote())
-			e.evaluated[e.tailpos()].evaluated = rewritten
+			rewritten.add(e.tail().evaluated...)
+			rewritten.add(tokenCQuote())
+			rewritten.addsrc(tokenCQuote())
+			e.evalstack[e.tailpos()] = rewritten
 		}
 
 		return nil
 	}
 
+	// handle advanced interpolation cases:
+	// - "${0 + 1}" and anything mathing ${<any> <op> <any>}
+	// - "${funcall(0)}"
+	// - "<string>${<anything>}<string>" and all variants.
+
+	// at this point the stack looks like:
+	//
+	//                           .
+	//                           . (nodePos - 1)
+	//                           . scratchPos (this string node)
+	//                           . 1st interpolation part
+	//                           . 2nd interpolation part
+	//                           . nth interpolation part
+	//
+	// The code below will merge all interpolation parts into this node.
+
+	// we merge subsequent string interpolation into previous (last) TokenQuotedLit.
 	var last *hclwrite.Token
-	for i := scratchPos + 1; i <= e.tailpos(); i++ {
-		switch e.evaluated[i].evaluated[0].Type {
+	for i := nodePos + 1; i <= e.tailpos(); i++ {
+		switch e.evalstack[i].evaluated[0].Type {
 		case hclsyntax.TokenOBrace, hclsyntax.TokenOBrack:
 			return errutil.Chain(
 				ErrInterpolationEval,
 				errorf("serialization of collection value is not supported"),
 			)
 		case hclsyntax.TokenQuotedLit:
-			if len(e.evaluated[i].evaluated) > 1 {
+			if len(e.evalstack[i].evaluated) > 1 {
 				panic("unexpected case")
 			}
 
-			rewritten = append(rewritten, e.evaluated[i].evaluated...)
-			last = rewritten[len(rewritten)-1]
+			rewritten.add(e.evalstack[i].evaluated...)
+			rewritten.addsrc(e.evalstack[i].source...)
+			last = rewritten.evaluated[len(rewritten.evaluated)-1]
 		case hclsyntax.TokenTemplateInterp:
-			rewritten = append(rewritten, e.evaluated[i].evaluated...)
-			last = rewritten[len(rewritten)-1]
+			rewritten.add(e.evalstack[i].evaluated...)
+			rewritten.addsrc(e.evalstack[i].source...)
+			last = rewritten.evaluated[len(rewritten.evaluated)-1]
 		case hclsyntax.TokenNumberLit, hclsyntax.TokenIdent:
-			if len(e.evaluated[i].evaluated) > 1 {
+			if len(e.evalstack[i].evaluated) > 1 {
 				panic("expects one part")
 			}
 
 			if last == nil {
-				rewritten = append(rewritten, &hclwrite.Token{
+				rewritten.add(&hclwrite.Token{
 					Type:  hclsyntax.TokenQuotedLit,
-					Bytes: e.evaluated[i].evaluated[0].Bytes,
+					Bytes: e.evalstack[i].evaluated[0].Bytes,
 				})
-				last = rewritten[len(rewritten)-1]
+				rewritten.addsrc(e.evalstack[i].source...)
+				last = rewritten.evaluated[len(rewritten.evaluated)-1]
 			} else {
-				last.Bytes = append(last.Bytes, e.evaluated[i].evaluated[0].Bytes...)
+				last.Bytes = append(last.Bytes, e.evalstack[i].evaluated[0].Bytes...)
 			}
 
 		case hclsyntax.TokenOQuote:
-			if len(e.evaluated[i].evaluated) != 3 {
+			if len(e.evalstack[i].evaluated) != 3 {
 				panic(sprintf(
 					"unexpected string case: %s (%d)",
-					e.evaluated[i].evaluated.Bytes(), len(e.evaluated[i].evaluated)))
+					e.evalstack[i].evaluated.Bytes(), len(e.evalstack[i].evaluated)))
 			}
 
 			if last == nil {
-				rewritten = append(rewritten, &hclwrite.Token{
+				rewritten.add(&hclwrite.Token{
 					Type:  hclsyntax.TokenQuotedLit,
-					Bytes: e.evaluated[i].evaluated[1].Bytes,
+					Bytes: e.evalstack[i].evaluated[1].Bytes,
 				})
-				last = rewritten[len(rewritten)-1]
+				rewritten.addsrc(e.evalstack[i].source...)
+				last = rewritten.evaluated[len(rewritten.evaluated)-1]
 			} else {
-				last.Bytes = append(last.Bytes, e.evaluated[i].evaluated[1].Bytes...)
+				last.Bytes = append(last.Bytes, e.evalstack[i].evaluated[1].Bytes...)
 			}
 
 		default:
 			panic(sprintf("unexpected interpolation type: %s (%s)",
-				e.evaluated[i].evaluated[0].Bytes, e.evaluated[i].evaluated[0].Type))
+				e.evalstack[i].evaluated[0].Bytes, e.evalstack[i].evaluated[0].Type))
 		}
 	}
 
-	rewritten = append(rewritten, tokenCQuote())
-	e.evaluated[scratchPos].evaluated = rewritten
-	e.evaluated = e.evaluated[e.headpos() : scratchPos+1]
+	rewritten.add(tokenCQuote())
+	rewritten.addsrc(tokenCQuote())
+	e.evalstack[nodePos] = rewritten
+	e.evalstack = e.evalstack[e.headpos() : nodePos+1]
 
 	return nil
 }
@@ -1135,7 +1180,7 @@ func (e *engine) canEvaluateIdent() bool {
 }
 
 func (e *engine) headpos() int {
-	if len(e.evaluated) == 0 {
+	if len(e.evalstack) == 0 {
 		panic("no evaluated elements")
 	}
 	return 0
@@ -1143,8 +1188,8 @@ func (e *engine) headpos() int {
 
 func (e *engine) tailpos() int {
 	var pos int
-	if len(e.evaluated) > 0 {
-		pos = len(e.evaluated) - 1
+	if len(e.evalstack) > 0 {
+		pos = len(e.evalstack) - 1
 	} else {
 		pos = 0
 	}
@@ -1196,6 +1241,7 @@ func isTmFuncall(tok *hclwrite.Token) bool {
 }
 
 func isSameTokens(a, b hclwrite.Tokens) bool {
+	fmt.Printf("len(a): %d, len(b): %d\n", len(a), len(b))
 	if len(a) != len(b) {
 		return false
 	}
@@ -1203,9 +1249,11 @@ func isSameTokens(a, b hclwrite.Tokens) bool {
 	for i := 0; i < len(a); i++ {
 		if b[i].Type != a[i].Type ||
 			string(b[i].Bytes) != string(a[i].Bytes) {
+			fmt.Printf("a[%d] != b[%d]: `%s` != `%s`\n", i, i, a[i].Bytes, b[i].Bytes)
 			return false
 		}
 	}
+	fmt.Printf("are the same\n")
 	return true
 }
 
