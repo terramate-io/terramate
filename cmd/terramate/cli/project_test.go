a8e816f5c815897aeba4265a7402aac6da1de3d4
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-05-03 18:53:06 +0200

    test: remove unneeded git instance

--- cmd/terramate/cli/project_test.go
+++ cmd/terramate/cli/project_test.go
@@ -19,7 +19,6 @@ import (
 
 	"github.com/madlambda/spells/assert"
 	"github.com/mineiros-io/terramate/errors"
-	"github.com/mineiros-io/terramate/test"
 	"github.com/mineiros-io/terramate/test/sandbox"
 	"github.com/rs/zerolog"
 )
@@ -62,8 +61,7 @@ func TestLocalDefaultIsOutdated(t *testing.T) {
 
 	assert.NoError(t, prj.setDefaults(&cliSpec{}))
 
-	g := test.NewGitWrapper(t, s.RootDir(), []string{})
-	assert.IsError(t, prj.checkLocalDefaultIsUpdated(g), errors.E(ErrOutdatedLocalRev))
+	assert.IsError(t, prj.checkLocalDefaultIsUpdated(), errors.E(ErrOutdatedLocalRev))
 }
 
 func init() {
