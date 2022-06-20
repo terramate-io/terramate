f45baa0ef9da508da28a16d8e0ec616c11d15007
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-06-20 21:19:47 +0200

    refactor: extract running commands to run pkg

--- cmd/terramate/cli/cli.go
+++ cmd/terramate/cli/cli.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
@@ -935,40 +934,21 @@ func (c *cli) runOnStacks() {
 
 	logger.Info().Msg("Running on selected stacks")
 
-	failed := false
+	err = run.Exec(
+		orderedStacks,
+		c.parsedArgs.Run.Command[0],
+		c.parsedArgs.Run.Command[1:],
+		c.stdin,
+		c.stdout,
+		c.stderr,
+		c.parsedArgs.Run.ContinueOnError,
+	)
 
-	for _, stack := range orderedStacks {
-		cmd := exec.Command(c.parsedArgs.Run.Command[0], c.parsedArgs.Run.Command[1:]...)
-		cmd.Dir = stack.HostPath()
-		cmd.Env = os.Environ()
-		cmd.Stdin = c.stdin
-		cmd.Stdout = c.stdout
-		cmd.Stderr = c.stderr
+	if err != nil {
+		logger.Warn().
+			Err(err).
+			Msg("failed to execute commands")
 
-		logger := log.With().
-			Str("cmd", strings.Join(c.parsedArgs.Run.Command, " ")).
-			Stringer("stack", stack).
-			Logger()
-
-		logger.Info().Msg("Running")
-
-		err = cmd.Run()
-		if err != nil {
-			failed = true
-
-			if c.parsedArgs.Run.ContinueOnError {
-				logger.Warn().
-					Err(err).
-					Msg("failed to execute command")
-			} else {
-				logger.Fatal().
-					Err(err).
-					Msg("failed to execute command")
-			}
-		}
-	}
-
-	if failed {
 		os.Exit(1)
 	}
 }
---
+++ run/exec.go
@@ -0,0 +1,83 @@
+// Copyright 2022 Mineiros GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package run
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/mineiros-io/terramate/errors"
+	"github.com/mineiros-io/terramate/stack"
+	"github.com/rs/zerolog/log"
+)
+
+// Exec will execute the given command on the given stack list
+// During the execution of this function the default behavior
+// for signal handling will be changed. The behavior implemented is:
+//
+// 1 x CTRL-C -> graceful shutdown, current stack executes but no other stack is executed.
+// No signal is forwarded to the sub process.
+// 2 x CTRL-C -> forward CTRL-C to running sub process
+// 3 x CTRL-C -> forward CTRL-C to running sub process 2nd time
+// 4 x CTRL-C -> kill running sub process with SIGKILL
+//
+// If continue on error is true this function will continue to execute
+// commands on stacks even in face of failures, returning an error.L of errors.
+// If continue on error is false it will return as soon as it finds an error,
+// returning a list with a single error inside.
+func Exec(
+	stacks []stack.S,
+	cmd string,
+	args []string,
+	stdin io.Reader,
+	stdout io.Writer,
+	stderr io.Writer,
+	continueOnError bool,
+) error {
+
+	logger := log.With().
+		Str("action", "run.Exec()").
+		Str("cmd", strings.Join(append([]string{cmd}, args...), " ")).
+		Logger()
+
+	errs := errors.L()
+
+	for _, stack := range stacks {
+		cmd := exec.Command(cmd, args...)
+		cmd.Dir = stack.HostPath()
+		cmd.Env = os.Environ()
+		cmd.Stdin = stdin
+		cmd.Stdout = stdout
+		cmd.Stderr = stderr
+
+		logger := logger.With().
+			Stringer("stack", stack).
+			Logger()
+
+		logger.Info().Msg("Running")
+
+		err := cmd.Run()
+		errs.Append(err)
+		if err != nil {
+			if !continueOnError {
+				return errs.AsError()
+			}
+		}
+	}
+
+	return errs.AsError()
+}
