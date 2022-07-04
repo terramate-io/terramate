a4ab1e6bbffc2f4b78654a76a2771f01c4cda4fc
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-07-04 14:56:21 +0200

    chore: add initial test plan / proto

---
+++ stack/create.go
@@ -0,0 +1,10 @@
+package stack
+
+// Create creates a stack on the provided stackdir.
+// Any dirs on the path provided by stackdir that doesn't exist will be created.
+//
+// If the stack already exists it will return with no error and no changes
+// will be performed on the stack.
+func Create(root, stackdir string) error {
+	return nil
+}
---
+++ stack/create_test.go
@@ -0,0 +1,10 @@
+package stack_test
+
+// TODO(katcipis)
+//
+// - Dot dir is not allowed
+// - Dir exists
+// - Dir + stack.tm.hcl exists (no stack config, only file)
+// - Dir already exists and dirs inside are not stack
+// - Dir with stack already exists (file is not stack.tm.hcl)
+// - Dir with other configs but no stack definition and no stack.tm.hcl
