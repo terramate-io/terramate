fa9073ab592a4e83fec0b05eaac7f487cb257319
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-07-15 21:09:58 +0200

    test: add initial test for clone

--- cmd/terramate/e2etests/exp_clone_test.go
+++ cmd/terramate/e2etests/exp_clone_test.go
@@ -15,12 +15,92 @@
 package e2etest
 
 import (
+	"fmt"
+	"path/filepath"
 	"testing"
 
+	"github.com/madlambda/spells/assert"
+	"github.com/mineiros-io/terramate/test"
 	"github.com/mineiros-io/terramate/test/sandbox"
 )
 
 func TestCloneStack(t *testing.T) {
+	const (
+		srcStack         = "stack"
+		destStack        = "cloned-stack"
+		stackID          = "stack-id"
+		stackName        = "stack name"
+		stackDesc        = "stack description"
+		stackCfgFilename = "stack.tm.hcl"
+		stackCfgTemplate = `
+// Commenting generate_hcl 1
+generate_hcl "test.hcl" {
+  content {
+    // Commenting literal
+    a = "literal"
+    // Commenting expression
+    b = tm_try(global.expression, null)
+  }
+}
+// Some comments
+/*
+  Commenting is fun
+*/
+stack{
+  // Commenting stack ID
+  id = %q // comment after ID expression
+  // Commenting stack name
+  name = %q // More comments !!
+  // Commenting stack description
+  description = %q
+}
+generate_hcl "test2.hcl" {
+  content {
+    b = tm_try(global.expression, null)
+    a = "literal"
+  }
+}
+`
+	)
 	s := sandbox.New(t)
+	s.BuildTree([]string{"d:stack"})
+
+	stackEntry := s.DirEntry("stack")
+	stackEntry.CreateFile(stackCfgFilename, fmt.Sprintf(stackCfgTemplate,
+		stackID, stackName, stackDesc))
+
 	tmcli := newCLI(t, s.RootDir())
+	res := tmcli.run("experimental", "clone", srcStack, destStack)
+
+	assertRunResult(t, res, runExpected{
+		Stdout: fmt.Sprintf("Cloned stack %s to %s with success\n", srcStack, destStack),
+	})
+
+	destdir := filepath.Join(s.RootDir(), destStack)
+	cfg := test.ParseTerramateConfig(t, destdir)
+
+	if cfg.Stack == nil {
+		t.Fatalf("cloned stack has no stack block: %v", cfg)
+	}
+
+	clonedStackID, ok := cfg.Stack.ID.Value()
+	if !ok {
+		t.Fatalf("cloned stack has no ID: %v", cfg.Stack)
+	}
+
+	if clonedStackID == stackID {
+		t.Fatalf("want cloned stack to have different ID, got %s == %s", clonedStackID, stackID)
+	}
+
+	assert.EqualStrings(t, stackName, cfg.Stack.Name)
+	assert.EqualStrings(t, stackDesc, cfg.Stack.Description)
+
+	want := fmt.Sprintf(stackCfgTemplate, clonedStackID, stackName, stackDesc)
+
+	clonedStackEntry := s.DirEntry(destStack)
+	got := string(clonedStackEntry.ReadFile(stackCfgFilename))
+
+	assert.EqualStrings(t, want, got, "want:\n%s\ngot:\n%s\n", want, got)
+
+	// Checking that code was also generated already
 }
