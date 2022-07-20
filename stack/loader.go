e7f074520b37f96c3822c3ea526ac88a1dbb4827
Tiago de Bem Natel de Moura <t.nateldemoura@gmail.com>
2022-07-20 21:07:15 +0100

    chore: use stack.S pointer for future mutations. (#488)

--- stack/loader.go
+++ stack/loader.go
@@ -26,28 +26,28 @@ import (
 // Loader is a stack loader.
 type Loader struct {
 	root   string
-	stacks map[string]S
+	stacks map[string]*S
 }
 
 // NewLoader creates a new stack loader for project's root directory.
 func NewLoader(root string) Loader {
 	return Loader{
 		root:   root,
-		stacks: make(map[string]S),
+		stacks: make(map[string]*S),
 	}
 }
 
 // Load loads a stack from dir directory.
 // The provided directory must be an absolute path to the stack dir.
 // If the stack was previously loaded, it returns the cached one.
-func (l Loader) Load(dir string) (S, error) {
+func (l Loader) Load(dir string) (*S, error) {
 	stack, found, err := l.TryLoad(dir)
 	if err != nil {
-		return S{}, err
+		return nil, err
 	}
 
 	if !found {
-		return S{}, fmt.Errorf("directory %q is not a stack", dir)
+		return nil, fmt.Errorf("directory %q is not a stack", dir)
 	}
 
 	return stack, nil
@@ -56,7 +56,7 @@ func (l Loader) Load(dir string) (S, error) {
 // TryLoad tries to load a stack from directory. It returns found as true
 // only in the case that path contains a stack and it was correctly parsed.
 // It caches the stack for later use.
-func (l Loader) TryLoad(dir string) (stack S, found bool, err error) {
+func (l Loader) TryLoad(dir string) (stack *S, found bool, err error) {
 	logger := log.With().
 		Str("action", "Loader.TryLoad()").
 		Str("dir", dir).
@@ -70,8 +70,12 @@ func (l Loader) TryLoad(dir string) (stack S, found bool, err error) {
 	}
 
 	stack, found, err = TryLoad(l.root, dir)
+	if err != nil {
+		return nil, found, err
+	}
+
 	if !found {
-		return S{}, found, err
+		return nil, false, nil
 	}
 
 	l.stacks[stack.Path()] = stack
@@ -80,7 +84,7 @@ func (l Loader) TryLoad(dir string) (stack S, found bool, err error) {
 
 // TryLoadChanged is like TryLoad but sets the stack as changed if loaded
 // successfully.
-func (l Loader) TryLoadChanged(root, dir string) (stack S, found bool, err error) {
+func (l Loader) TryLoadChanged(root, dir string) (stack *S, found bool, err error) {
 	logger := log.With().
 		Str("action", "TryLoadChanged()").
 		Str("stack", dir).
@@ -98,19 +102,19 @@ func (l Loader) TryLoadChanged(root, dir string) (stack S, found bool, err error
 
 // Set stacks in the loader's cache. The dir directory must be relative to
 // project's root.
-func (l Loader) Set(dir string, s S) {
+func (l Loader) Set(dir string, s *S) {
 	l.stacks[dir] = s
 }
 
 // LoadAll loads all the stacks in the dirs paths. For each dir in dirs:
 // - If it is relative, it will be considered relative to wd, path = wd + dir
 // - If it is absolute, it will be considered absolute in relation to the given root, path = root + dir
-func (l Loader) LoadAll(root string, wd string, dirs ...string) ([]S, error) {
+func (l Loader) LoadAll(root string, wd string, dirs ...string) (List, error) {
 	logger := log.With().
 		Str("action", "LoadAll()").
 		Logger()
 
-	stacks := []S{}
+	stacks := List{}
 
 	logger.Trace().
 		Str("path", root).
@@ -136,9 +140,9 @@ func (l Loader) LoadAll(root string, wd string, dirs ...string) ([]S, error) {
 	return stacks, nil
 }
 
-func (l Loader) lookupParentStack(dir string) (stack S, found bool, err error) {
+func (l Loader) lookupParentStack(dir string) (stack *S, found bool, err error) {
 	if l.root == dir {
-		return S{}, false, nil
+		return nil, false, nil
 	}
 	d := filepath.Dir(dir)
 	for {
@@ -149,7 +153,7 @@ func (l Loader) lookupParentStack(dir string) (stack S, found bool, err error) {
 			Msg("Try load directory.")
 		stack, ok, err := l.TryLoad(d)
 		if err != nil {
-			return S{}, false, fmt.Errorf("looking for parent stacks: %w", err)
+			return nil, false, fmt.Errorf("looking for parent stacks: %w", err)
 		}
 
 		if ok {
@@ -173,5 +177,5 @@ func (l Loader) lookupParentStack(dir string) (stack S, found bool, err error) {
 		d = filepath.Dir(d)
 	}
 
-	return S{}, false, nil
+	return nil, false, nil
 }
