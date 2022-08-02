c60c3bd91e2f45e01182d41fea04c766bfa09f2a
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-08-02 15:13:50 +0200

    feat: add initial vendor source parsing

--- module/vendor.go
+++ module/vendor.go
@@ -14,6 +14,28 @@
 
 package module
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mineiros-io/terramate/errors"
+)
+
+// Source represents a module source
+type Source struct {
+	// Remote is the remote of the source that will be referenced
+	// directly when downloading a source.
+	Remote string
+
+	// Ref is the specific reference of this source, if any.
+	Ref string
+}
+
+const (
+	// ErrUnsupportedModSrc indicates that a module source string is invalid.
+	ErrUnsupportedModSrc errors.Kind = "unsupported module source"
+)
+
 // Vendor will vendor the given module inside the provided vendor
 // dir. If the project is already vendored it will do nothing and return
 // as a success.
@@ -23,11 +45,15 @@ package module
 // - <vendordir>/<domain>/<dir>/<subdir>/<ref>
 //
 // The whole path inside the vendor dir will be created if it not exists.
+// Vendoring is not recursive, so dependencies won't have their dependencies vendored.
+// Vendoring will also not download git submodules, if any.
 //
-// The module source must be a valid Terraform source reference as documented in:
+// The module source must be a valid Terraform Git/Github source reference as documented in:
 //
 // - https://www.terraform.io/language/modules/sources
-func Vendor(vendordir, modsource string) error {
+//
+// Source references that are not Git/Github are not supported.
+func Vendor(vendordir, src Source) error {
 	// Use: https://pkg.go.dev/github.com/hashicorp/go-getter#Detect
 	// To detect modsource to a valid URL and then extract the path/reference
 	// from the URL, so we can define the final path inside vendor.
@@ -37,4 +63,39 @@ func Vendor(vendordir, modsource string) error {
 	//
 	// More details: https://github.com/hashicorp/terraform/blob/main/internal/getmodules/getter.go#L14
 	return nil
+}
+
+// ParseSource parses the given modsource string. It returns an error if the modsource
+// string is unsupported.
+func ParseSource(modsource string) (Source, error) {
+	ref := ""
+	splitParams := strings.Split(modsource, "?")
+	if len(splitParams) > 1 {
+		// TODO(katcipis): handle invalid multiple ??
+		// TODO(katcipis): handle missing ref
+		// TODO(katcipis): handle extra params
+		ref = strings.Split(splitParams[1], "=")[1]
+	}
+	modsource = splitParams[0]
+
+	switch {
+	case strings.HasPrefix(modsource, "github.com"):
+		return Source{
+			Remote: fmt.Sprintf("https://%s.git", modsource),
+			Ref:    ref,
+		}, nil
+	case strings.HasPrefix(modsource, "git@"):
+		return Source{
+			Remote: modsource,
+			Ref:    ref,
+		}, nil
+	case strings.HasPrefix(modsource, "git::"):
+		return Source{
+			Remote: strings.TrimPrefix(modsource, "git::"),
+			Ref:    ref,
+		}, nil
+
+	default:
+		return Source{}, errors.E(ErrUnsupportedModSrc)
+	}
 }
--- module/vendor_test.go
+++ module/vendor_test.go
@@ -14,7 +14,165 @@
 
 package module_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/madlambda/spells/assert"
+	"github.com/mineiros-io/terramate/errors"
+	"github.com/mineiros-io/terramate/module"
+)
 
 func TestModVendor(t *testing.T) {
+}
+
+func TestParseGitSources(t *testing.T) {
+	type want struct {
+		parsed module.Source
+		err    error
+	}
+
+	type testcase struct {
+		name   string
+		source string
+		want   want
+	}
+
+	tcases := []testcase{
+		{
+			name:   "github source",
+			source: "github.com/mineiros-io/example",
+			want: want{
+				parsed: module.Source{
+					Remote: "https://github.com/mineiros-io/example.git",
+				},
+			},
+		},
+		{
+			name:   "github source with ref",
+			source: "github.com/mineiros-io/example?ref=v1",
+			want: want{
+				parsed: module.Source{
+					Remote: "https://github.com/mineiros-io/example.git",
+					Ref:    "v1",
+				},
+			},
+		},
+		{
+			name:   "git@ source",
+			source: "git@github.com:mineiros-io/example.git",
+			want: want{
+				parsed: module.Source{
+					Remote: "git@github.com:mineiros-io/example.git",
+				},
+			},
+		},
+		{
+			name:   "git@ source with ref",
+			source: "git@github.com:mineiros-io/example.git?ref=v2",
+			want: want{
+				parsed: module.Source{
+					Remote: "git@github.com:mineiros-io/example.git",
+					Ref:    "v2",
+				},
+			},
+		},
+		{
+			name:   "git::https source",
+			source: "git::https://example.com/vpc.git",
+			want: want{
+				parsed: module.Source{
+					Remote: "https://example.com/vpc.git",
+				},
+			},
+		},
+		{
+			name:   "git::https source with ref",
+			source: "git::https://example.com/vpc.git?ref=v3",
+			want: want{
+				parsed: module.Source{
+					Remote: "https://example.com/vpc.git",
+					Ref:    "v3",
+				},
+			},
+		},
+		{
+			name:   "git::ssh source",
+			source: "git::ssh://username@example.com/storage.git",
+			want: want{
+				parsed: module.Source{
+					Remote: "ssh://username@example.com/storage.git",
+				},
+			},
+		},
+		{
+			name:   "git::ssh source with ref",
+			source: "git::ssh://username@example.com/storage.git?ref=v4",
+			want: want{
+				parsed: module.Source{
+					Remote: "ssh://username@example.com/storage.git",
+					Ref:    "v4",
+				},
+			},
+		},
+		{
+			name:   "https is not supported",
+			source: "https://example.com/vpc-module.zip",
+			want: want{
+				err: errors.E(module.ErrUnsupportedModSrc),
+			},
+		},
+		{
+			name:   "hg is not supported",
+			source: "hg::http://example.com/vpc.hg",
+			want: want{
+				err: errors.E(module.ErrUnsupportedModSrc),
+			},
+		},
+		{
+			name:   "terraform registry is not supported",
+			source: "hashicorp/consul/aws",
+			want: want{
+				err: errors.E(module.ErrUnsupportedModSrc),
+			},
+		},
+		{
+			name:   "registry is not supported",
+			source: "app.terraform.io/example-corp/k8s-cluster/azurerm",
+			want: want{
+				err: errors.E(module.ErrUnsupportedModSrc),
+			},
+		},
+		{
+			name:   "bitbucket is not supported",
+			source: "bitbucket.org/hashicorp/terraform-consul-aws",
+			want: want{
+				err: errors.E(module.ErrUnsupportedModSrc),
+			},
+		},
+		{
+			name:   "gcs is not supported",
+			source: "gcs::https://www.googleapis.com/storage/v1/modules/foomodule.zip",
+			want: want{
+				err: errors.E(module.ErrUnsupportedModSrc),
+			},
+		},
+		{
+			name:   "s3 is not supported",
+			source: "s3::https://s3-eu-west-1.amazonaws.com/examplecorp-terraform-modules/vpc.zip",
+			want: want{
+				err: errors.E(module.ErrUnsupportedModSrc),
+			},
+		},
+	}
+
+	for _, tcase := range tcases {
+		t.Run(tcase.name, func(t *testing.T) {
+			parsed, err := module.ParseSource(tcase.source)
+			assert.IsError(t, err, tcase.want.err)
+			if tcase.want.err != nil {
+				return
+			}
+			assert.Partial(t, parsed, tcase.want.parsed)
+		})
+	}
 }
