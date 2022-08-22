bc2a0c28ecec2763a144c42eafe8adf08bbff33e
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-08-22 20:17:51 +0200

    chore: improve generate_file test

--- generate/genfile/genfile_test.go
+++ generate/genfile/genfile_test.go
@@ -142,8 +142,7 @@ func TestLoadGenerateFiles(t *testing.T) {
 						labels("test"),
 						expr("content", `<<EOT
 
-stacks_list_len=${tm_length(terramate.stacks.list)}
-stacks_list_0=${terramate.stacks.list[0]}
+stacks_list=${tm_jsonencode(terramate.stacks.list)}
 stack_path_abs=${terramate.stack.path.absolute}
 stack_path_rel=${terramate.stack.path.relative}
 stack_path_to_root=${terramate.stack.path.to_root}
@@ -162,8 +161,7 @@ EOT`,
 					file: genFile{
 						origin: "/stack/test.tm",
 						body: `
-stacks_list_len=1
-stacks_list_0=/stack
+stacks_list=["/stack"]
 stack_path_abs=/stack
 stack_path_rel=stack
 stack_path_to_root=..
