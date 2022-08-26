537459d12381d184c199c9d16ba8369f3240376a
Tiago de Bem Natel de Moura <t.nateldemoura@gmail.com>
2022-08-26 16:51:53 +0100

    fix: panic when parsing terraform files with empty module.source (#558)

--- tf/terraform.go
+++ tf/terraform.go
@@ -41,7 +41,8 @@ const (
 // IsLocal tells if module source is a local directory.
 func (m Module) IsLocal() bool {
 	// As specified here: https://www.terraform.io/docs/language/modules/sources.html#local-paths
-	return m.Source[0:2] == "./" || m.Source[0:3] == "../"
+	return (len(m.Source) >= 2 && m.Source[0:2] == "./") ||
+		(len(m.Source) >= 3 && m.Source[0:3] == "../")
 }
 
 // ParseModules parses blocks of type "module" containing a single label.
