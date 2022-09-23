9a2ae10d67d50a8882a0fd609621cc209f22c61d
i4k <t.nateldemoura@gmail.com>
2022-09-23 01:06:41 +0100

    add deps check

--- makefiles/windows/1.mk
+++ makefiles/windows/1.mk
@@ -8,3 +8,7 @@ endif
 
 VERSION ?= v$(shell type VERSION)
 GOLANGCI_LINT_VERSION ?= v1.49.0
+
+DEPS = awk git go gcc
+$(foreach dep,$(DEPS),\
+    $(if $(shell where $(dep)),,$(error "Program $(dep) not found in PATH")))
