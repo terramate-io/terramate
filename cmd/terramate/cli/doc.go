e961f7da11ff34b7858bd0e5e470ebcd8764c9fa
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-09-28 14:39:35 +0200

    chore: add license info

--- cmd/terramate/cli/doc.go
+++ cmd/terramate/cli/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package modvendor provides Terraform module vendoring support.
-package modvendor
+// Package cli provides cli specific functionality.
+package cli
--- fs/doc.go
+++ fs/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package modvendor provides Terraform module vendoring support.
-package modvendor
+// Package fs provides filesystem related functionality.
+package fs
