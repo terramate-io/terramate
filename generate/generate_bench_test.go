7c7a5e0dd3c8c4d638875b33d061f0c08be9ddd0
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-10-06 19:53:47 +0200

    test: add initial bench scaffold and automation

---
+++ generate/generate_bench_test.go
@@ -0,0 +1,70 @@
+// Copyright 2022 Mineiros GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package generate_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkGenerate(b *testing.B) {
+	benchs := []benchmark{
+		{
+			stacks:   10,
+			asserts:  10,
+			genhcl:   10,
+			genfiles: 5,
+			globals:  100,
+		},
+		{
+			stacks:   100,
+			asserts:  10,
+			genhcl:   10,
+			genfiles: 5,
+			globals:  100,
+		},
+		{
+			stacks:   1000,
+			asserts:  10,
+			genhcl:   10,
+			genfiles: 5,
+			globals:  100,
+		},
+	}
+
+	for _, bench := range benchs {
+		b.Run(bench.String(), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				bench.run(b)
+			}
+		})
+	}
+}
+
+type benchmark struct {
+	stacks   int
+	asserts  int
+	genhcl   int
+	genfiles int
+	globals  int
+}
+
+func (bm benchmark) String() string {
+	return fmt.Sprintf("stacks=%d asserts=%d genhcl=%d genfiles=%d globals=%d",
+		bm.stacks, bm.asserts, bm.genhcl, bm.genfiles, bm.globals)
+}
+
+func (bm benchmark) run(b *testing.B) {
+}
