49fb5e5f4bc51f03ede9c0b82f1965ed85b8a0f7
Tiago de Bem Natel de Moura <t.nateldemoura@gmail.com>
2022-10-31 14:37:39 +0000

    fix: make check_remote safeguard run from any branch (#681)

--- cmd/terramate/e2etests/general_test.go
+++ cmd/terramate/e2etests/general_test.go
@@ -286,7 +286,7 @@ func TestBaseRefFlagPrecedenceOverDefault(t *testing.T) {
 	)
 }
 
-func TestFailsOnChangeDetectionIfCurrentBranchIsMainAndItIsOutdated(t *testing.T) {
+func TestFailsOnRunIfRemoteMainIsOutdated(t *testing.T) {
 	t.Parallel()
 
 	s := sandbox.New(t)
@@ -312,13 +312,39 @@ func TestFailsOnChangeDetectionIfCurrentBranchIsMainAndItIsOutdated(t *testing.T
 
 	assertRunResult(t, ts.listChangedStacks(), wantRes)
 
-	assertRunResult(t, ts.run(
-		"run",
-		"--changed",
-		testHelperBin,
-		"cat",
-		mainTfFile.HostPath(),
-	), wantRes)
+	testrun := func() {
+		assertRunResult(t, ts.run(
+			"run",
+			testHelperBin,
+			"cat",
+			mainTfFile.HostPath(),
+		), wantRes)
+
+		assertRunResult(t, ts.run(
+			"run",
+			"--changed",
+			testHelperBin,
+			"cat",
+			mainTfFile.HostPath(),
+		), wantRes)
+	}
+
+	testrun()
+
+	git.CheckoutNew("branch")
+
+	// we create two commits so we can also test from a DETACHED HEAD.
+	stack.CreateFile("tempfile2", "any content")
+	git.CommitAll("add tempfile2")
+
+	stack.CreateFile("tempfile3", "any content")
+	git.CommitAll("add tempfile3")
+
+	testrun()
+
+	git.Checkout("HEAD^1")
+
+	testrun()
 }
 
 func TestMainAfterOriginMainMustUseDefaultBaseRef(t *testing.T) {
