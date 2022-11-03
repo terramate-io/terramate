e339ce1a45e306289daef1600716f91410aeec1d
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-11-03 12:37:47 +0100

    chore: add license info

--- modvendor/event_test.go
+++ modvendor/event_test.go
@@ -12,18 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package modvendor
-
-import "github.com/mineiros-io/terramate/event"
-
-// ProgressEvent represents a vendor progress event.
-type ProgressEvent struct {
-}
-
-// EventStream is a stream of vendor related events.
-type EventStream event.Stream[ProgressEvent]
-
-// NewEventStream creates a new event stream.
-func NewEventStream() EventStream {
-	return EventStream(event.NewStream[ProgressEvent](100))
-}
+package modvendor_test
