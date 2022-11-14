cb44ab3c3f225a5a7e7a7f426fa81369d83e6160
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-11-14 20:53:04 +0100

    test: add first failing test for tm_vendor integration

--- generate/vendor_test.go
+++ generate/vendor_test.go
@@ -14,4 +14,102 @@
 
 package generate_test
 
-// TODO(KATCIPIS): test event stream provides the expected events with the proper events
+import (
+	"fmt"
+	"testing"
+
+	"github.com/mineiros-io/terramate/generate"
+	. "github.com/mineiros-io/terramate/test/hclwrite/hclutils"
+)
+
+func TestGenerateVendor(t *testing.T) {
+	t.Parallel()
+
+	testCodeGeneration(t, []testcase{
+		{
+			name: "tm_vendor path is relative to generate blocks label",
+			layout: []string{
+				"s:stacks/stack",
+				"s:stacks/stack/substack",
+			},
+			vendorDir: "/vendor",
+			configs: []hclconfig{
+				{
+					path: "/stacks",
+					add: Doc(
+						GenerateHCL(
+							Labels("file.hcl"),
+							Content(
+								Expr("vendor", `tm_vendor("github.com/mineiros-io/terramate?ref=v1")`),
+							),
+						),
+						GenerateFile(
+							Labels("file.txt"),
+							Expr("content", `tm_vendor("github.com/mineiros-io/terramate?ref=v2")`),
+						),
+						GenerateHCL(
+							Labels("dir/file.hcl"),
+							Content(
+								Expr("vendor", `tm_vendor("github.com/mineiros-io/terramate?ref=v3")`),
+							),
+						),
+						GenerateFile(
+							Labels("dir/file.txt"),
+							Expr("content", `tm_vendor("github.com/mineiros-io/terramate?ref=v4")`),
+						),
+					),
+				},
+			},
+			want: []generatedFile{
+				{
+					stack: "/stacks/stack",
+					files: map[string]fmt.Stringer{
+						"dir/file.hcl": Doc(
+							Str("vendor", "../../../vendor/github.com/mineiros-io/terramate/v3"),
+						),
+						"dir/file.txt": stringer("../../../vendor/github.com/mineiros-io/terramate/v4"),
+						"file.hcl": Doc(
+							Str("vendor", "../../vendor/github.com/mineiros-io/terramate/v1"),
+						),
+						"file.txt": stringer("../../vendor/github.com/mineiros-io/terramate/v2"),
+					},
+				},
+				{
+					stack: "/stacks/stack/substack",
+					files: map[string]fmt.Stringer{
+						"dir/file.hcl": Doc(
+							Str("vendor", "../../../../vendor/github.com/mineiros-io/terramate/v3"),
+						),
+						"dir/file.txt": stringer("../../../../vendor/github.com/mineiros-io/terramate/v4"),
+						"file.hcl": Doc(
+							Str("vendor", "../../../vendor/github.com/mineiros-io/terramate/v1"),
+						),
+						"file.txt": stringer("../../../vendor/github.com/mineiros-io/terramate/v2"),
+					},
+				},
+			},
+			wantReport: generate.Report{
+				Successes: []generate.Result{
+					{
+						Dir: "/stacks/stack",
+						Created: []string{
+							"dir/file.hcl",
+							"dir/file.txt",
+							"file.hcl",
+							"file.txt",
+						},
+					},
+					{
+						Dir: "/stacks/stack/substack",
+						Created: []string{
+							"dir/file.hcl",
+							"dir/file.txt",
+							"file.hcl",
+							"file.txt",
+						},
+					},
+				},
+			},
+		},
+	})
+}
