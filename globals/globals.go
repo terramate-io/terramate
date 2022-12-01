a8a2f186d219dadc4f7105bb54f5d2757349adae
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-12-01 19:47:18 +0100

    chore: add initial possible solution

--- globals/globals.go
+++ globals/globals.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/mineiros-io/terramate/hcl/eval"
 	"github.com/mineiros-io/terramate/project"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/zclconf/go-cty/cty"
 )
@@ -267,6 +268,50 @@ func (le loadedExprs) eval(ctx *eval.Context) EvalReport {
 		})
 	}
 
+	parentHasObjectBeingExtended := func(
+		logger zerolog.Logger,
+		accessor GlobalPathKey,
+		hierarchicalLevel int,
+	) bool {
+		if hierarchicalLevel == 0 {
+			logger.Trace().Msg("on root level, no need to check parents")
+			return false
+		}
+		childAccPath := accessor.Path()
+		if len(childAccPath) <= 1 {
+			logger.Trace().Msg("acessor <= 1, no need to check parents")
+			return false
+		}
+
+		// TODO(KATCIPIS): still not working 100%
+		//for i := hierarchicalLevel - 1; i >= 0; i-- {
+		//parentGlobalAccessors := sortedGlobalAccessors[i]
+
+		//searchParent:
+		//for _, parentAccessor := range parentGlobalAccessors.accessors {
+		//parentAccPath := parentAccessor.Path()
+		//// More specific definitions (bigger) on the parent
+		//// are overrided by the child definition.
+		//if len(parentAccPath) > len(childAccPath) {
+		//continue
+		//}
+
+		//for i, parentPathElem := range parentAccPath {
+		//childPathElem := childAccPath[i]
+		//if childPathElem != parentPathElem {
+		//continue searchParent
+		//}
+		//}
+
+		//logger.Trace().
+		//Stringer("parent", parentGlobalAccessors.origin).
+		//Msg("parent has object being extended by child")
+		//return true
+		//}
+		//}
+		return false
+	}
+
 	if !ctx.HasNamespace("global") {
 		ctx.SetNamespace("global", map[string]cty.Value{})
 	}
@@ -276,7 +321,7 @@ func (le loadedExprs) eval(ctx *eval.Context) EvalReport {
 
 		logger.Trace().Msg("evaluating pending expressions")
 
-		for _, sortedGlobals := range sortedGlobalAccessors {
+		for hierarchicalLevel, sortedGlobals := range sortedGlobalAccessors {
 
 		pendingExpression:
 			for _, accessor := range sortedGlobals.accessors {
@@ -349,6 +394,14 @@ func (le loadedExprs) eval(ctx *eval.Context) EvalReport {
 							"global.%s attribute redefined: previously defined at %s",
 							accessor.rootname(), oldValue.Origin().String()))
 
+					continue
+				}
+
+				// This is to avoid setting a label defined extension on the child
+				// and later overwriting that with an object definition on the parent
+
+				if parentHasObjectBeingExtended(logger, accessor, hierarchicalLevel) {
+					logger.Debug().Msg("parent defines object being extended, delaying eval")
 					continue
 				}
 
