terramate-io/terramate

commit d2c147d51e03f71da6860bba2e3df9424f2826df

chore: remove project.Metadata and config.StackMetadata in favor of simple project.Runtime (#788)

cmd/terramate/cli/cli.go
@@ -982,14 +982,6 @@ func (c *cli) printStacks() {
 	}
 }
 
-func (c *cli) newProjectMetadata(report *terramate.StacksReport) prj.Metadata {
-	stacks := make(config.List[*config.Stack], len(report.Stacks))
-	for i, stackEntry := range report.Stacks {
-		stacks[i] = stackEntry.Stack
-	}
-	return config.NewProjectMetadata(c.rootdir(), stacks)
-}
-
 func (c *cli) printRunEnv() {
 	mgr := terramate.NewManager(c.cfg(), c.prj.baseRef)
 	report, err := c.listStacks(mgr, c.parsedArgs.Changed)
@@ -997,10 +989,8 @@ func (c *cli) printRunEnv() {
 		fatal(err, "listing stacks")
 	}
 
-	projmeta := c.newProjectMetadata(report)
-
 	for _, stackEntry := range c.filterStacksByWorkingDir(report.Stacks) {
-		envVars, err := run.LoadEnv(c.cfg(), projmeta, stackEntry.Stack)
+		envVars, err := run.LoadEnv(c.cfg(), stackEntry.Stack)
 		if err != nil {
 			fatal(err, "loading stack run environment")
 		}
@@ -1238,14 +1228,12 @@ func (c *cli) printStacksGlobals() {
 		fatal(err, "listing stacks globals: listing stacks")
 	}
 
-	projmeta := c.newProjectMetadata(report)
-
 	for _, stackEntry := range c.filterStacksByWorkingDir(report.Stacks) {
-		meta := config.StackMetadata(stackEntry.Stack)
-		report := globals.ForStack(c.cfg(), projmeta, meta)
+		stack := stackEntry.Stack
+		report := globals.ForStack(c.cfg(), stack)
 		if err := report.AsError(); err != nil {
 			logger := log.With().
-				Stringer("stack", meta.Dir()).
+				Stringer("stack", stack.Dir()).
 				Logger()
 
 			errlog.Fatal(logger, err, "listing stacks globals: loading stack")
@@ -1256,7 +1244,7 @@ func (c *cli) printStacksGlobals() {
 			continue
 		}
 
-		c.output.MsgStdOut("\nstack %q:", meta.Dir())
+		c.output.MsgStdOut("\nstack %q:", stack.Dir())
 		for _, line := range strings.Split(globalsStrRepr, "\n") {
 			c.output.MsgStdOut("\t%s", line)
 		}
@@ -1283,30 +1271,27 @@ func (c *cli) printMetadata() {
 		return
 	}
 
-	projmeta := c.newProjectMetadata(report)
-
 	c.output.MsgStdOut("Available metadata:")
-
 	c.output.MsgStdOut("\nproject metadata:")
-	c.output.MsgStdOut("\tterramate.stacks.list=%v", projmeta.Stacks())
+	c.output.MsgStdOut("\tterramate.stacks.list=%v", c.cfg().Stacks())
 
 	for _, stackEntry := range stackEntries {
-		stackMeta := config.StackMetadata(stackEntry.Stack)
+		stack := stackEntry.Stack
 
 		logger.Debug().
-			Stringer("stack", stackEntry.Stack).
+			Stringer("stack", stack).
 			Msg("Print metadata for individual stack.")
 
-		c.output.MsgStdOut("\nstack %q:", stackMeta.Dir())
-		if id, ok := stackMeta.ID(); ok {
+		c.output.MsgStdOut("\nstack %q:", stack.Dir())
+		if id, ok := stack.ID(); ok {
 			c.output.MsgStdOut("\tterramate.stack.id=%q", id)
 		}
-		c.output.MsgStdOut("\tterramate.stack.name=%q", stackMeta.Name())
-		c.output.MsgStdOut("\tterramate.stack.description=%q", stackMeta.Desc())
-		c.output.MsgStdOut("\tterramate.stack.path.absolute=%q", stackMeta.Dir())
-		c.output.MsgStdOut("\tterramate.stack.path.basename=%q", stackMeta.PathBase())
-		c.output.MsgStdOut("\tterramate.stack.path.relative=%q", stackMeta.RelPath())
-		c.output.MsgStdOut("\tterramate.stack.path.to_root=%q", stackMeta.RelPathToRoot(c.cfg()))
+		c.output.MsgStdOut("\tterramate.stack.name=%q", stack.Name())
+		c.output.MsgStdOut("\tterramate.stack.description=%q", stack.Desc())
+		c.output.MsgStdOut("\tterramate.stack.path.absolute=%q", stack.Dir())
+		c.output.MsgStdOut("\tterramate.stack.path.basename=%q", stack.PathBase())
+		c.output.MsgStdOut("\tterramate.stack.path.relative=%q", stack.RelPath())
+		c.output.MsgStdOut("\tterramate.stack.path.to_root=%q", stack.RelPathToRoot(c.cfg()))
 	}
 }
 
@@ -1422,21 +1407,17 @@ func (c *cli) outputEvalResult(val cty.Value, asJSON bool) {
 
 func (c *cli) setupEvalContext() *eval.Context {
 	ctx := eval.NewContext(stdlib.Functions(c.wd()))
-	allstacks, err := config.LoadAllStacks(c.cfg().Tree())
-	if err != nil {
-		fatal(err, "setup eval context: listing all stacks")
-	}
-
-	projmeta := config.NewProjectMetadata(c.rootdir(), allstacks)
+	runtime := c.cfg().RuntimeValues()
 	if config.IsStack(c.cfg(), c.wd()) {
 		st, err := config.LoadStack(c.cfg(), prj.PrjAbsPath(c.rootdir(), c.wd()))
 		if err != nil {
 			fatal(err, "setup eval context: loading stack config")
 		}
-		ctx.SetNamespace("terramate", stack.MetadataToCtyValues(c.cfg(), projmeta, st))
-	} else {
-		ctx.SetNamespace("terramate", projmeta.ToCtyMap())
+		runtime.Merge(st.RuntimeValues(c.cfg()))
+
 	}
+
+	ctx.SetNamespace("terramate", runtime)
 
 	globals.ForDir(c.cfg(), prj.PrjAbsPath(c.rootdir(), c.wd()), ctx)
 	return ctx

config/config.go
@@ -25,6 +25,7 @@ import (
 	"github.com/mineiros-io/terramate/hcl"
 	"github.com/mineiros-io/terramate/project"
 	"github.com/rs/zerolog/log"
+	"github.com/zclconf/go-cty/cty"
 )
 
 const (
@@ -225,6 +226,32 @@ func (root *Root) LoadSubTree(cfgdir project.Path) error {
 	return nil
 }
 
+// Stacks return the stacks paths.
+func (root *Root) Stacks() project.Paths {
+	return root.tree.Stacks().Paths()
+}
+
+// RuntimeValues returns the runtime terramate namespace for the root as a cty.Value map.
+func (root *Root) RuntimeValues() project.Runtime {
+	rootfs := cty.ObjectVal(map[string]cty.Value{
+		"absolute": cty.StringVal(root.HostDir()),
+		"basename": cty.StringVal(filepath.Base(root.HostDir())),
+	})
+	rootpath := cty.ObjectVal(map[string]cty.Value{
+		"fs": rootfs,
+	})
+	rootNS := cty.ObjectVal(map[string]cty.Value{
+		"path": rootpath,
+	})
+	stacksNs := cty.ObjectVal(map[string]cty.Value{
+		"list": toCtyStringList(root.Stacks().Strings()),
+	})
+	return project.Runtime{
+		"root":   rootNS,
+		"stacks": stacksNs,
+	}
+}
+
 // LoadTree loads the whole hierarchical configuration from cfgdir downwards
 // using rootdir as project root.
 func LoadTree(rootdir string, cfgdir string) (*Tree, error) {
@@ -417,4 +444,16 @@ func Skip(name string) bool {
 func parentDir(dir string) (string, bool) {
 	parent := filepath.Dir(dir)
 	return parent, parent != dir
+}
+
+func toCtyStringList(list []string) cty.Value {
+	if len(list) == 0 {
+		// cty panics if the list is empty
+		return cty.ListValEmpty(cty.String)
+	}
+	res := make([]cty.Value, len(list))
+	for i, elem := range list {
+		res[i] = cty.StringVal(elem)
+	}
+	return cty.ListVal(res)
 }

generate/generate.go
@@ -105,19 +105,18 @@ func Load(root *config.Root, vendorDir project.Path) ([]LoadResult, error) {
 	if err != nil {
 		return nil, err
 	}
-	projmeta := config.NewProjectMetadata(root.HostDir(), stacks)
 	results := make([]LoadResult, len(stacks))
 
 	for i, st := range stacks {
 		res := LoadResult{Dir: st.Dir()}
-		loadres := globals.ForStack(root, projmeta, st)
+		loadres := globals.ForStack(root, st)
 		if err := loadres.AsError(); err != nil {
 			res.Err = err
 			results[i] = res
 			continue
 		}
 
-		generated, err := loadStackCodeCfgs(root, projmeta, st, loadres.Globals, vendorDir, nil)
+		generated, err := loadStackCodeCfgs(root, st, loadres.Globals, vendorDir, nil)
 		if err != nil {
 			res.Err = err
 			results[i] = res
@@ -198,7 +197,6 @@ func Do(
 
 func doStackGeneration(
 	root *config.Root,
-	projmeta project.Metadata,
 	stack *config.Stack,
 	globals *eval.Object,
 	vendorDir project.Path,
@@ -214,7 +212,7 @@ func doStackGeneration(
 
 	logger.Debug().Msg("generating files")
 
-	generated, err := loadStackCodeCfgs(root, projmeta, stack, globals, vendorDir, vendorRequests)
+	generated, err := loadStackCodeCfgs(root, stack, globals, vendorDir, vendorRequests)
 	if err != nil {
 		report.err = err
 		return report
@@ -316,16 +314,9 @@ func doRootGeneration(root *config.Root) Report {
 		Str("action", "generate.doRootGeneration").
 		Logger()
 
-	stackConfigs := root.Tree().Stacks()
-	var stackpaths []project.Path
-	for _, stack := range stackConfigs {
-		stackpaths = append(stackpaths, stack.Dir())
-	}
-
 	report := Report{}
-	projmeta := project.NewMetadata(root.HostDir(), stackpaths)
 	evalctx := eval.NewContext(stdlib.Functions(root.HostDir()))
-	evalctx.SetNamespace("terramate", projmeta.ToCtyMap())
+	evalctx.SetNamespace("terramate", root.RuntimeValues())
 
 	var files []GenFile
 	for _, cfg := range root.Tree().AsList() {
@@ -518,15 +509,13 @@ func DetectOutdated(root *config.Root, vendorDir project.Path) ([]string, error)
 		return nil, err
 	}
 
-	projmeta := config.NewProjectMetadata(root.HostDir(), stacks)
-
 	outdatedFiles := []string{}
 	errs := errors.L()
 
 	logger.Debug().Msg("checking outdated code inside stacks")
 
 	for _, stack := range stacks {
-		outdated, err := stackOutdated(root, projmeta, stack, vendorDir)
+		outdated, err := stackOutdated(root, stack, vendorDir)
 		if err != nil {
 			errs.Append(err)
 			continue
@@ -571,7 +560,6 @@ func DetectOutdated(root *config.Root, vendorDir project.Path) ([]string, error)
 // If the stack has an invalid configuration it will return an error.
 func stackOutdated(
 	root *config.Root,
-	projmeta project.Metadata,
 	st *config.Stack,
 	vendorDir project.Path,
 ) ([]string, error) {
@@ -580,13 +568,13 @@ func stackOutdated(
 		Stringer("stack", st).
 		Logger()
 
-	report := globals.ForStack(root, projmeta, st)
+	report := globals.ForStack(root, st)
 	if err := report.AsError(); err != nil {
 		return nil, errors.E(err, "checking for outdated code")
 	}
 
 	globals := report.Globals
-	generated, err := loadStackCodeCfgs(root, projmeta, st, globals, vendorDir, nil)
+	generated, err := loadStackCodeCfgs(root, st, globals, vendorDir, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -788,7 +776,6 @@ func readFile(path string) (string, bool, error) {
 
 type forEachStackFunc func(
 	*config.Root,
-	project.Metadata,
 	*config.Stack,
 	*eval.Object,
 	project.Path,
@@ -816,25 +803,23 @@ func forEachStack(
 		return report
 	}
 
-	projmeta := config.NewProjectMetadata(root.HostDir(), stacks)
-
-	for _, st := range stacks {
+	for _, stack := range stacks {
 		logger := logger.With().
-			Stringer("stack", st).
+			Stringer("stack", stack).
 			Logger()
 
 		logger.Trace().Msg("Load stack globals.")
 
-		globalsReport := globals.ForStack(root, projmeta, st)
+		globalsReport := globals.ForStack(root, stack)
 		if err := globalsReport.AsError(); err != nil {
-			report.addFailure(st.Dir(), errors.E(ErrLoadingGlobals, err))
+			report.addFailure(stack.Dir(), errors.E(ErrLoadingGlobals, err))
 			continue
 		}
 
 		logger.Trace().Msg("Calling stack callback.")
 
-		stackReport := fn(root, projmeta, st, globalsReport.Globals, vendorDir, vendorRequests)
-		report.addDirReport(st.Dir(), stackReport)
+		stackReport := fn(root, stack, globalsReport.Globals, vendorDir, vendorRequests)
+		report.addDirReport(stack.Dir(), stackReport)
 	}
 
 	return report
@@ -1236,14 +1221,14 @@ func checkFileConflict(generated []GenFile) map[string]error {
 	return errsmap
 }
 
-func loadAsserts(root *config.Root, meta project.Metadata, sm config.StackMetadata, globals *eval.Object) ([]config.Assert, error) {
+func loadAsserts(root *config.Root, st *config.Stack, globals *eval.Object) ([]config.Assert, error) {
 	logger := log.With().
 		Str("action", "generate.loadAsserts").
 		Str("rootdir", root.HostDir()).
-		Str("stack", sm.Dir().String()).
+		Str("stack", st.Dir().String()).
 		Logger()
 
-	curdir := sm.Dir()
+	curdir := st.Dir()
 	asserts := []config.Assert{}
 	errs := errors.L()
 
@@ -1252,8 +1237,7 @@ func loadAsserts(root *config.Root, meta project.Metadata, sm config.StackMetada
 			Stringer("curdir", curdir).
 			Logger()
 
-		evalctx := stack.NewEvalCtx(root, meta, sm, globals)
-
+		evalctx := stack.NewEvalCtx(root, st, globals)
 		cfg, ok := root.Lookup(curdir)
 		if ok {
 			for _, assertCfg := range cfg.Node.Asserts {
@@ -1282,25 +1266,24 @@ func loadAsserts(root *config.Root, meta project.Metadata, sm config.StackMetada
 
 func loadStackCodeCfgs(
 	root *config.Root,
-	projmeta project.Metadata,
 	st *config.Stack,
 	globals *eval.Object,
 	vendorDir project.Path,
 	vendorRequests chan<- event.VendorRequest,
 ) ([]GenFile, error) {
-	asserts, err := loadAsserts(root, projmeta, st, globals)
+	asserts, err := loadAsserts(root, st, globals)
 	if err != nil {
 		return nil, err
 	}
 
 	var genfilesConfigs []GenFile
 
-	genfiles, err := genfile.Load(root, projmeta, st, globals, vendorDir, vendorRequests)
+	genfiles, err := genfile.Load(root, st, globals, vendorDir, vendorRequests)
 	if err != nil {
 		return nil, err
 	}
 
-	genhcls, err := genhcl.Load(root, projmeta, st, globals, vendorDir, vendorRequests)
+	genhcls, err := genhcl.Load(root, st, globals, vendorDir, vendorRequests)
 	if err != nil {
 		return nil, err
 	}

globals/stack.go
@@ -17,17 +17,16 @@ package globals
 import (
 	"github.com/mineiros-io/terramate/config"
 	"github.com/mineiros-io/terramate/hcl/eval"
-	"github.com/mineiros-io/terramate/project"
-	"github.com/mineiros-io/terramate/stack"
 	"github.com/mineiros-io/terramate/stdlib"
 )
 
 // ForStack loads from the config tree all globals defined for a given stack.
-func ForStack(root *config.Root, projmeta project.Metadata, stackmeta config.StackMetadata) EvalReport {
+func ForStack(root *config.Root, stack *config.Stack) EvalReport {
 	ctx := eval.NewContext(
-		stdlib.Functions(stackmeta.HostDir(root)),
+		stdlib.Functions(stack.HostDir(root)),
 	)
-	ctx.SetNamespace("terramate", stack.MetadataToCtyValues(root,
-		projmeta, stackmeta))
-	return ForDir(root, stackmeta.Dir(), ctx)
+	runtime := root.RuntimeValues()
+	runtime.Merge(stack.RuntimeValues(root))
+	ctx.SetNamespace("terramate", runtime)
+	return ForDir(root, stack.Dir(), ctx)
 }

stack/eval.go
@@ -19,9 +19,7 @@ import (
 
 	"github.com/mineiros-io/terramate/config"
 	"github.com/mineiros-io/terramate/hcl/eval"
-	"github.com/mineiros-io/terramate/project"
 	"github.com/mineiros-io/terramate/stdlib"
-	"github.com/rs/zerolog/log"
 	"github.com/zclconf/go-cty/cty"
 )
 
@@ -33,13 +31,13 @@ type EvalCtx struct {
 }
 
 // NewEvalCtx creates a new stack evaluation context.
-func NewEvalCtx(root *config.Root, projmeta project.Metadata, sm config.StackMetadata, globals *eval.Object) *EvalCtx {
-	evalctx := eval.NewContext(stdlib.Functions(sm.HostDir(root)))
+func NewEvalCtx(root *config.Root, stack *config.Stack, globals *eval.Object) *EvalCtx {
+	evalctx := eval.NewContext(stdlib.Functions(stack.HostDir(root)))
 	evalwrapper := &EvalCtx{
 		Context: evalctx,
 		root:    root,
 	}
-	evalwrapper.SetMetadata(projmeta, sm)
+	evalwrapper.SetMetadata(stack)
 	evalwrapper.SetGlobals(globals)
 	return evalwrapper
 }
@@ -50,8 +48,10 @@ func (e *EvalCtx) SetGlobals(g *eval.Object) {
 }
 
 // SetMetadata sets the given metadata on the stack evaluation context.
-func (e *EvalCtx) SetMetadata(projmeta project.Metadata, sm config.StackMetadata) {
-	e.SetNamespace("terramate", MetadataToCtyValues(e.root, projmeta, sm))
+func (e *EvalCtx) SetMetadata(st *config.Stack) {
+	runtime := e.root.RuntimeValues()
+	runtime.Merge(st.RuntimeValues(e.root))
+	e.SetNamespace("terramate", runtime)
 }
 
 // SetEnv sets the given environment on the env namespace of the evaluation context.
@@ -63,56 +63,4 @@ func (e *EvalCtx) SetEnv(environ []string) {
 		env[parsed[0]] = cty.StringVal(parsed[1])
 	}
 	e.SetNamespace("env", env)
-}
-
-func stackMetaToCtyMap(root *config.Root, m config.StackMetadata) map[string]cty.Value {
-	logger := log.With().
-		Str("action", "stack.stackMetaToCtyMap()").
-		Logger()
-
-	logger.Trace().Msg("creating stack metadata")
-
-	stackpath := cty.ObjectVal(map[string]cty.Value{
-		"absolute": cty.StringVal(m.Dir().String()),
-		"relative": cty.StringVal(m.RelPath()),
-		"basename": cty.StringVal(m.PathBase()),
-		"to_root":  cty.StringVal(m.RelPathToRoot(root)),
-	})
-	stackMapVals := map[string]cty.Value{
-		"name":        cty.StringVal(m.Name()),
-		"description": cty.StringVal(m.Desc()),
-		"path":        stackpath,
-	}
-	if id, ok := m.ID(); ok {
-		logger.Trace().
-			Str("id", id).
-			Msg("adding stack ID to metadata")
-
-		stackMapVals["id"] = cty.StringVal(id)
-	}
-	stack := cty.ObjectVal(stackMapVals)
-	return map[string]cty.Value{
-		"name":        cty.StringVal(m.Name()),         // DEPRECATED
-		"path":        cty.StringVal(m.Dir().String()), // DEPRECATED
-		"description": cty.StringVal(m.Desc()),         // DEPRECATED
-		"stack":       stack,
-	}
-}
-
-// MetadataToCtyValues converts the metadatas to a map of cty.Values.
-func MetadataToCtyValues(root *config.Root, projmeta project.Metadata, sm config.StackMetadata) map[string]cty.Value {
-	projvalues := projmeta.ToCtyMap()
-	stackvalues := stackMetaToCtyMap(root, sm)
-
-	tmvar := map[string]cty.Value{}
-	for k, v := range projvalues {
-		tmvar[k] = v
-	}
-	for k, v := range stackvalues {
-		if _, ok := tmvar[k]; ok {
-			panic("project metadata and stack metadata conflicts")
-		}
-		tmvar[k] = v
-	}
-	return tmvar
 }

config/stack.go
@@ -24,6 +24,7 @@ import (
 	"github.com/mineiros-io/terramate/hcl"
 	"github.com/mineiros-io/terramate/project"
 	"github.com/rs/zerolog/log"
+	"github.com/zclconf/go-cty/cty"
 )
 
 type (
@@ -60,26 +61,6 @@ type (
 
 		// changed tells if this is a changed stack.
 		changed bool
-	}
-
-	// StackMetadata has all metadata loaded per stack
-	StackMetadata interface {
-		// ID of the stack if it has any. Empty string and false otherwise.
-		ID() (string, bool)
-		// Name of the stack.
-		Name() string
-		// HostDir is the absolute path of the stack on the host file system.
-		HostDir(*Root) string
-		// Dir is the absolute path of the stack (relative to project root).
-		Dir() project.Path
-		// RelPath is the relative path of the from root.
-		RelPath() string
-		// PathBase is the basename of the stack path.
-		PathBase() string
-		// Desc is the description of the stack (relative to project root).
-		Desc() string
-		// RelPathToRoot is the relative path from the stack to root.
-		RelPathToRoot(*Root) string
 	}
 )
 
@@ -186,6 +167,41 @@ func (s *Stack) HostDir(root *Root) string {
 	return project.AbsPath(root.HostDir(), s.Dir().String())
 }
 
+// RuntimeValues returns the runtime "terramate" namespace for the stack.
+func (s *Stack) RuntimeValues(root *Root) map[string]cty.Value {
+	logger := log.With().
+		Str("action", "stack.stackMetaToCtyMap()").
+		Logger()
+
+	logger.Trace().Msg("creating stack metadata")
+
+	stackpath := cty.ObjectVal(map[string]cty.Value{
+		"absolute": cty.StringVal(s.Dir().String()),
+		"relative": cty.StringVal(s.RelPath()),
+		"basename": cty.StringVal(s.PathBase()),
+		"to_root":  cty.StringVal(s.RelPathToRoot(root)),
+	})
+	stackMapVals := map[string]cty.Value{
+		"name":        cty.StringVal(s.Name()),
+		"description": cty.StringVal(s.Desc()),
+		"path":        stackpath,
+	}
+	if id, ok := s.ID(); ok {
+		logger.Trace().
+			Str("id", id).
+			Msg("adding stack ID to metadata")
+
+		stackMapVals["id"] = cty.StringVal(id)
+	}
+	stack := cty.ObjectVal(stackMapVals)
+	return map[string]cty.Value{
+		"name":        cty.StringVal(s.Name()),         // DEPRECATED
+		"path":        cty.StringVal(s.Dir().String()), // DEPRECATED
+		"description": cty.StringVal(s.Desc()),         // DEPRECATED
+		"stack":       stack,
+	}
+}
+
 func validateWatchPaths(rootdir string, stackpath string, paths []string) (project.Paths, error) {
 	var projectPaths project.Paths
 	for _, pathstr := range paths {
@@ -226,15 +242,6 @@ func StacksFromTrees(root string, trees List[*Tree]) (List[*Stack], error) {
 		stacks = append(stacks, s)
 	}
 	return stacks, nil
-}
-
-// NewProjectMetadata creates project metadata from a given rootdir and a list of stacks.
-func NewProjectMetadata(rootdir string, stacks List[*Stack]) project.Metadata {
-	stackPaths := make(project.Paths, len(stacks))
-	for i, st := range stacks {
-		stackPaths[i] = st.Dir()
-	}
-	return project.NewMetadata(rootdir, stackPaths)
 }
 
 // LoadAllStacks loads all stacks inside the given rootdir.
@@ -318,7 +325,7 @@ func ReverseStacks(stacks List[*Stack]) {
 	}
 }
 
-// Paths returns the project paths from the stack list.
+// Paths returns the project paths from the list.
 func (l List[T]) Paths() project.Paths {
 	paths := make(project.Paths, len(l))
 	for i, s := range l {

generate/genfile/genfile.go
@@ -130,13 +130,12 @@ func (f File) String() string {
 // The rootdir MUST be an absolute path.
 func Load(
 	root *config.Root,
-	projmeta project.Metadata,
-	sm config.StackMetadata,
+	st *config.Stack,
 	globals *eval.Object,
 	vendorDir project.Path,
 	vendorRequests chan<- event.VendorRequest,
 ) ([]File, error) {
-	genFileBlocks, err := loadGenFileBlocks(root, sm.Dir())
+	genFileBlocks, err := loadGenFileBlocks(root, st.Dir())
 	if err != nil {
 		return nil, errors.E("loading generate_file", err)
 	}
@@ -149,9 +148,9 @@ func Load(
 		}
 
 		name := genFileBlock.Label
-		evalctx := stack.NewEvalCtx(root, projmeta, sm, globals)
+		evalctx := stack.NewEvalCtx(root, st, globals)
 		vendorTargetDir := project.NewPath(path.Join(
-			sm.Dir().String(),
+			st.Dir().String(),
 			path.Dir(name)))
 
 		evalctx.SetFunction(stdlib.Name("vendor"), stdlib.VendorFunc(vendorTargetDir, vendorDir, vendorRequests))

generate/genfile/genfile_test.go
@@ -970,8 +970,6 @@ func testGenfile(t *testing.T, tcase testcase) {
 
 		s := sandbox.New(t)
 		s.BuildTree([]string{"s:" + tcase.stack})
-		stacks := s.LoadStacks()
-		projmeta := config.NewProjectMetadata(s.RootDir(), stacks)
 		stack := s.LoadStacks()[0]
 
 		for _, cfg := range tcase.configs {
@@ -986,9 +984,9 @@ func testGenfile(t *testing.T, tcase testcase) {
 
 		assert.NoError(t, err)
 
-		globals := s.LoadStackGlobals(root, projmeta, stack)
+		globals := s.LoadStackGlobals(root, stack)
 		vendorDir := project.NewPath("/modules")
-		got, err := genfile.Load(root, projmeta, stack, globals, vendorDir, nil)
+		got, err := genfile.Load(root, stack, globals, vendorDir, nil)
 		errtest.Assert(t, err, tcase.wantErr)
 
 		if len(got) != len(tcase.want) {

commit be3cbe1591229f3a662c53f80710e3392da3e840

chore: fix performance regression introduced by #788

cmd/terramate/cli/cli.go
@@ -1417,7 +1417,7 @@ func (c *cli) outputEvalResult(val cty.Value, asJSON bool) {
 
 func (c *cli) setupEvalContext() *eval.Context {
 	ctx := eval.NewContext(stdlib.Functions(c.wd()))
-	runtime := c.cfg().RuntimeValues()
+	runtime := c.cfg().Runtime()
 	if config.IsStack(c.cfg(), c.wd()) {
 		st, err := config.LoadStack(c.cfg(), prj.PrjAbsPath(c.rootdir(), c.wd()))
 		if err != nil {

config/config.go
@@ -47,6 +47,8 @@ const (
 // configuration for the root directory is expected and not from anywhere else.
 type Root struct {
 	tree Tree
+
+	runtime project.Runtime
 }
 
 // Tree is the configuration tree.
@@ -115,9 +117,11 @@ func TryLoadConfig(fromdir string) (tree *Root, configpath string, found bool, e
 
 // NewRoot creates a new [Root] tree for the cfg tree.
 func NewRoot(tree *Tree) *Root {
-	return &Root{
+	r := &Root{
 		tree: *tree,
 	}
+	r.initRuntime()
+	return r
 }
 
 // LoadRoot loads the root configuration tree.
@@ -246,8 +250,17 @@ func (root *Root) Stacks() project.Paths {
 	return root.tree.Stacks().Paths()
 }
 
-// RuntimeValues returns the runtime terramate namespace for the root as a cty.Value map.
-func (root *Root) RuntimeValues() project.Runtime {
+// Runtime returns a copy the runtime for the root terramate namespace as a
+// cty.Value map.
+func (root *Root) Runtime() project.Runtime {
+	runtime := project.Runtime{}
+	for k, v := range root.runtime {
+		runtime[k] = v
+	}
+	return runtime
+}
+
+func (root *Root) initRuntime() {
 	rootfs := cty.ObjectVal(map[string]cty.Value{
 		"absolute": cty.StringVal(root.HostDir()),
 		"basename": cty.StringVal(filepath.Base(root.HostDir())),
@@ -261,7 +274,7 @@ func (root *Root) RuntimeValues() project.Runtime {
 	stacksNs := cty.ObjectVal(map[string]cty.Value{
 		"list": toCtyStringList(root.Stacks().Strings()),
 	})
-	return project.Runtime{
+	root.runtime = project.Runtime{
 		"root":   rootNS,
 		"stacks": stacksNs,
 	}

generate/generate.go
@@ -316,7 +316,7 @@ func doRootGeneration(root *config.Root) Report {
 
 	report := Report{}
 	evalctx := eval.NewContext(stdlib.Functions(root.HostDir()))
-	evalctx.SetNamespace("terramate", root.RuntimeValues())
+	evalctx.SetNamespace("terramate", root.Runtime())
 
 	var files []GenFile
 	for _, cfg := range root.Tree().AsList() {

globals/stack.go
@@ -25,7 +25,7 @@ func ForStack(root *config.Root, stack *config.Stack) EvalReport {
 	ctx := eval.NewContext(
 		stdlib.Functions(stack.HostDir(root)),
 	)
-	runtime := root.RuntimeValues()
+	runtime := root.Runtime()
 	runtime.Merge(stack.RuntimeValues(root))
 	ctx.SetNamespace("terramate", runtime)
 	return ForDir(root, stack.Dir, ctx)

stack/eval.go
@@ -49,7 +49,7 @@ func (e *EvalCtx) SetGlobals(g *eval.Object) {
 
 // SetMetadata sets the given metadata on the stack evaluation context.
 func (e *EvalCtx) SetMetadata(st *config.Stack) {
-	runtime := e.root.RuntimeValues()
+	runtime := e.root.Runtime()
 	runtime.Merge(st.RuntimeValues(e.root))
 	e.SetNamespace("terramate", runtime)
 }