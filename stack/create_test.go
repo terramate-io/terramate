350f57e9a363635e3555bd8911d285d11cc44d66
Tiago Natel <t.nateldemoura@gmail.com>
2023-02-06 14:19:34 +0000

    remove duplicated from merge

--- stack/create_test.go
+++ stack/create_test.go
@@ -120,22 +120,6 @@ func TestStackCreation(t *testing.T) {
 			},
 		},
 		{
-			name: "defining invalid after",
-			stack: config.Stack{
-				Dir:   project.NewPath("/stack-after"),
-				After: []string{"stack-1", "stack-1"},
-			},
-			want: want{err: errors.E(config.ErrStackValidation)},
-		},
-		{
-			name: "defining invalid before",
-			stack: config.Stack{
-				Dir:    project.NewPath("/stack-after"),
-				Before: []string{"stack-1", "stack-1"},
-			},
-			want: want{err: errors.E(config.ErrStackValidation)},
-		},
-		{
 			name: "defining duplicated tags - fails",
 			stack: config.Stack{
 				Dir:  project.NewPath("/stack-with-tags"),
@@ -156,7 +140,7 @@ func TestStackCreation(t *testing.T) {
 		{
 			name: "defining invalid before",
 			stack: config.Stack{
-				Dir:    project.NewPath("/stack-after"),
+				Dir:    project.NewPath("/stack-before"),
 				Before: []string{"stack-1", "stack-1"},
 			},
 			want: want{err: errors.E(config.ErrStackValidation)},
