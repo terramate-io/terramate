35eb5547e98632671e56ee734904964249825170
Tiago Natel <t.nateldemoura@gmail.com>
2023-04-24 17:03:45 +0100

    chore: add benchmark for tm_regex() and generate

--- generate/generate_bench_test.go
+++ generate/generate_bench_test.go
@@ -78,3 +78,58 @@ func BenchmarkGenerate(b *testing.B) {
 		}
 	}
 }
+
+func BenchmarkGenerateRegex(b *testing.B) {
+	// benchmarks the case when there are a lot of tm_regex() reusing with
+	// patterns. The tm_regex() function must cache its compiled patterns.
+
+	b.StopTimer()
+	s := sandbox.New(b)
+
+	stackPaths := []string{
+		"s1",
+		"s1/s2",
+		"s1/s2/s3",
+		"s1/s2/s3/s4",
+		"s1/s2/s3/s4/s5",
+	}
+
+	layout := []string{}
+	for _, s := range stackPaths {
+		layout = append(layout, "s:"+s)
+	}
+
+	s.BuildTree(layout)
+
+	const numGlobalsPerStack = 100
+	for _, sp := range stackPaths {
+		content := `globals {
+			list = tm_range(1000)
+			pattern = "([\\d]+)-(\\d\\d)-(\\d\\d)"
+		`
+		for i := 0; i < numGlobalsPerStack; i++ {
+			content += fmt.Sprintf("\t%s_%d = [for i in global.list : tm_regex(global.pattern, \"${i*i}-01-01\")[0] == \"${i*i}\" ? i*i : -1]\n", path.Base(sp), i)
+		}
+		content += "}\n"
+		s.DirEntry(sp).CreateFile("globals.tm", content)
+	}
+
+	s.DirEntry(stackPaths[len(stackPaths)-1]).CreateFile("gen.tm", fmt.Sprintf(`
+	generate_hcl "leaf.hcl" {
+		content {
+			a = global.s5_%d
+		}
+	}
+	`, numGlobalsPerStack/2))
+
+	root, err := config.LoadRoot(s.RootDir())
+	assert.NoError(b, err)
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		report := generate.Do(root, project.NewPath("/vendor"), nil)
+		if report.HasFailures() {
+			b.Fatal(report.Full())
+		}
+	}
+}
