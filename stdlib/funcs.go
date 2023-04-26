a6a6eb7e488350931fac75442486875e0cfb8588
i4ki <t.nateldemoura@gmail.com>
2023-04-26 11:43:40 +0100

    chore: improve performance by caching the tm_regex() patterns. (#910)

--- stdlib/funcs.go
+++ stdlib/funcs.go
@@ -15,8 +15,12 @@
 package stdlib
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+
+	resyntax "regexp/syntax"
 
 	"github.com/hashicorp/hcl/v2/ext/customdecode"
 	tflang "github.com/hashicorp/terraform/lang"
@@ -31,6 +35,12 @@ import (
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
 )
+
+var regexCache map[string]*regexp.Regexp
+
+func init() {
+	regexCache = map[string]*regexp.Regexp{}
+}
 
 // Functions returns all the Terramate default functions.
 // The `basedir` must be an absolute path for an existent directory or it panics.
@@ -55,6 +65,9 @@ func Functions(basedir string) map[string]function.Function {
 		tmfuncs["tm_"+name] = function
 	}
 
+	// optimized regex
+	tmfuncs["tm_regex"] = Regex()
+
 	// fix terraform broken abspath()
 	tmfuncs["tm_abspath"] = AbspathFunc(basedir)
 
@@ -63,7 +76,151 @@ func Functions(basedir string) map[string]function.Function {
 
 	tmfuncs["tm_version_match"] = VersionMatch()
 	return tmfuncs
+}
 
+// Regex is a copy of Terraform [stdlib.RegexFunc] but with cached compiled
+// patterns.
+func Regex() function.Function {
+	return function.New(&function.Spec{
+		Params: []function.Parameter{
+			{
+				Name: "pattern",
+				Type: cty.String,
+			},
+			{
+				Name: "string",
+				Type: cty.String,
+			},
+		},
+		Type: func(args []cty.Value) (cty.Type, error) {
+			if !args[0].IsKnown() {
+				// We can't predict our type without seeing the pattern
+				return cty.DynamicPseudoType, nil
+			}
+
+			retTy, err := regexPatternResultType(args[0].AsString())
+			if err != nil {
+				err = function.NewArgError(0, err)
+			}
+			return retTy, err
+		},
+		Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
+			if retType == cty.DynamicPseudoType {
+				return cty.DynamicVal, nil
+			}
+
+			re, ok := regexCache[args[0].AsString()]
+			if !ok {
+				panic("should be in the cache")
+			}
+
+			str := args[1].AsString()
+
+			captureIdxs := re.FindStringSubmatchIndex(str)
+			if captureIdxs == nil {
+				return cty.NilVal, fmt.Errorf("pattern did not match any part of the given string")
+			}
+
+			return regexPatternResult(re, str, captureIdxs, retType), nil
+		},
+	})
+}
+
+// regexPatternResultType parses the given regular expression pattern and
+// returns the structural type that would be returned to represent its
+// capture groups.
+//
+// Returns an error if parsing fails or if the pattern uses a mixture of
+// named and unnamed capture groups, which is not permitted.
+func regexPatternResultType(pattern string) (cty.Type, error) {
+	re, ok := regexCache[pattern]
+	if !ok {
+		var rawErr error
+		re, rawErr = regexp.Compile(pattern)
+		switch err := rawErr.(type) {
+		case *resyntax.Error:
+			return cty.NilType, fmt.Errorf("invalid regexp pattern: %s in %s", err.Code, err.Expr)
+		case error:
+			// Should never happen, since all regexp compile errors should
+			// be resyntax.Error, but just in case...
+			return cty.NilType, fmt.Errorf("error parsing pattern: %s", err)
+		}
+
+		regexCache[pattern] = re
+	}
+
+	allNames := re.SubexpNames()[1:]
+	var names []string
+	unnamed := 0
+	for _, name := range allNames {
+		if name == "" {
+			unnamed++
+		} else {
+			if names == nil {
+				names = make([]string, 0, len(allNames))
+			}
+			names = append(names, name)
+		}
+	}
+	switch {
+	case unnamed == 0 && len(names) == 0:
+		// If there are no capture groups at all then we'll return just a
+		// single string for the whole match.
+		return cty.String, nil
+	case unnamed > 0 && len(names) > 0:
+		return cty.NilType, fmt.Errorf("invalid regexp pattern: cannot mix both named and unnamed capture groups")
+	case unnamed > 0:
+		// For unnamed captures, we return a tuple of them all in order.
+		etys := make([]cty.Type, unnamed)
+		for i := range etys {
+			etys[i] = cty.String
+		}
+		return cty.Tuple(etys), nil
+	default:
+		// For named captures, we return an object using the capture names
+		// as keys.
+		atys := make(map[string]cty.Type, len(names))
+		for _, name := range names {
+			atys[name] = cty.String
+		}
+		return cty.Object(atys), nil
+	}
+}
+
+func regexPatternResult(re *regexp.Regexp, str string, captureIdxs []int, retType cty.Type) cty.Value {
+	switch {
+	case retType == cty.String:
+		start, end := captureIdxs[0], captureIdxs[1]
+		return cty.StringVal(str[start:end])
+	case retType.IsTupleType():
+		captureIdxs = captureIdxs[2:] // index 0 is the whole pattern span, which we ignore by skipping one pair
+		vals := make([]cty.Value, len(captureIdxs)/2)
+		for i := range vals {
+			start, end := captureIdxs[i*2], captureIdxs[i*2+1]
+			if start < 0 || end < 0 {
+				vals[i] = cty.NullVal(cty.String) // Did not match anything because containing group didn't match
+				continue
+			}
+			vals[i] = cty.StringVal(str[start:end])
+		}
+		return cty.TupleVal(vals)
+	case retType.IsObjectType():
+		captureIdxs = captureIdxs[2:] // index 0 is the whole pattern span, which we ignore by skipping one pair
+		vals := make(map[string]cty.Value, len(captureIdxs)/2)
+		names := re.SubexpNames()[1:]
+		for i, name := range names {
+			start, end := captureIdxs[i*2], captureIdxs[i*2+1]
+			if start < 0 || end < 0 {
+				vals[name] = cty.NullVal(cty.String) // Did not match anything because containing group didn't match
+				continue
+			}
+			vals[name] = cty.StringVal(str[start:end])
+		}
+		return cty.ObjectVal(vals)
+	default:
+		// Should never happen
+		panic(fmt.Sprintf("invalid return type %#v", retType))
+	}
 }
 
 // Name converts the function name into the exported Terramate name.
