4329688ca8a34bb5fe5bb009a8b96460ac21fe9d
Tiago Natel <t.nateldemoura@gmail.com>
2023-04-26 14:57:49 +0100

    add licenses

--- hcl/eval/stmt.go
+++ hcl/eval/stmt.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Mineiros GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package eval
 
 import (
@@ -142,33 +156,25 @@ func (stmts Stmts) SelectBy(ref Ref, atChild map[RefStr]Ref) (Stmts, bool) {
 	isContainedBy := Stmts{}
 outer:
 	for _, stmt := range stmts {
-		//fmt.Printf("%s has %s: %t (scope %s): ", stmt.LHS, ref, stmt.LHS.Has(ref), stmt.Info.Scope)
-
 		if !stmt.Special {
 			for _, gotRef := range atChild {
 				if stmt.LHS.Has(gotRef) {
-					//fmt.Printf("ignored because origin %s already found in lower levels\n", gotRef)
 					continue outer
 				}
 			}
 		}
 
 		if stmt.LHS.Has(ref) {
-			//fmt.Printf("contains\n")
 			contains = append(contains, stmt)
 			if stmt.Origin.equal(ref) || stmt.LHS.equal(ref) {
 				found = true
 			}
 		} else {
 			if found {
-				//fmt.Printf("found\n")
 				return contains, true
 			}
 			if ref.Has(stmt.LHS) {
-				//fmt.Printf("is contained\n")
 				isContainedBy = append(isContainedBy, stmt)
-			} else {
-				//fmt.Printf("unrelated\n")
 			}
 		}
 	}
