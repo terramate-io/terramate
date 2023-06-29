1f877b1fe6aaed63c232884a9c2b16dbd55e0c95
Tiago Natel <t.nateldemoura@gmail.com>
2023-06-29 15:10:23 +0100

    fix: missing https:// in the baseURL

--- cmd/terramate/cli/github/client.go
+++ cmd/terramate/cli/github/client.go
@@ -48,7 +48,7 @@ func (c *Client) PullsForCommit(ctx context.Context, repository, commit string) 
 		return nil, errors.E("expects a valid Github repository of format <owner>/<name>")
 	}
 
-	url := fmt.Sprintf("%s/repos/%s/commits/%s/pulls", c.baseURL(), repository, commit)
+	url := fmt.Sprintf("https://%s/repos/%s/commits/%s/pulls", c.baseURL(), repository, commit)
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return nil, errors.E(err, "creating pulls request")
