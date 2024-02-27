ef69b363782b3109910963bbddca36966f980ca9
Wasif Malik <wmalik@gmail.com>
2024-02-27 14:11:46 +0100

    chore: remove git safeguards from list

--- cmd/terramate/cli/cli.go
+++ cmd/terramate/cli/cli.go
@@ -1602,7 +1602,6 @@ func (c *cli) printStacks() {
 	if err != nil {
 		fatal("Unable to list stacks", err)
 	}
-	c.gitFileSafeguards(false)
 
 	c.printStacksList(report.Stacks, c.parsedArgs.List.Why, c.parsedArgs.List.RunOrder)
 }
