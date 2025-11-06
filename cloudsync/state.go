e4edc56402df24a62db1dfbe7c89674c9d185802
Enrique J. Hernández <sixstone-qq@users.noreply.github.com>
2025-11-06 16:32:05 +0100

    fix: Break race condition using --parallel
    To create the map and update the map entries.
    
    By now, it's only the case of drifts but potentially it may affect
    deployments and previews in new API protocols

--- cloudsync/state.go
+++ cloudsync/state.go
@@ -5,6 +5,7 @@ package cloudsync
 
 import (
 	"strings"
+	"sync"
 
 	"github.com/terramate-io/terramate/cloud/api/resources"
 	"github.com/terramate-io/terramate/errors"
@@ -21,13 +22,15 @@ const (
 
 // CloudRunState represents the state of the current run.
 type CloudRunState struct {
+	sync.RWMutex
+
 	RunUUID resources.UUID
 
-	StackMeta2ID map[string]int64
-	// StackMeta2PreviewIDs is a map of stack.ID to stackPreview.ID
-	StackMeta2PreviewIDs map[string]string
-	// StackMeta2DriftUUIDs is a map of stack.ID to drift.UUID
-	StackMeta2DriftUUIDs map[string]resources.UUID
+	stackMeta2ID map[string]int64
+	// stackMeta2PreviewIDs is a map of stack.ID to stackPreview.ID
+	stackMeta2PreviewIDs map[string]string
+	// stackMeta2DriftUUIDs is a map of stack.ID to drift.UUID
+	stackMeta2DriftUUIDs map[string]resources.UUID
 	ReviewRequest        *resources.ReviewRequest
 	RREvent              struct {
 		PushedAt  *int64
@@ -38,43 +41,55 @@ type CloudRunState struct {
 
 // SetMeta2CloudID sets the cloud ID of a stack given its metadata ID.
 func (rs *CloudRunState) SetMeta2CloudID(metaID string, id int64) {
-	if rs.StackMeta2ID == nil {
-		rs.StackMeta2ID = make(map[string]int64)
+	rs.Lock()
+	defer rs.Unlock()
+	if rs.stackMeta2ID == nil {
+		rs.stackMeta2ID = make(map[string]int64)
 	}
-	rs.StackMeta2ID[strings.ToLower(metaID)] = id
+	rs.stackMeta2ID[strings.ToLower(metaID)] = id
 }
 
 // StackCloudID returns the cloud ID of a stack given its metadata ID.
-func (rs CloudRunState) StackCloudID(metaID string) (int64, bool) {
-	id, ok := rs.StackMeta2ID[strings.ToLower(metaID)]
+func (rs *CloudRunState) StackCloudID(metaID string) (int64, bool) {
+	rs.RLock()
+	defer rs.RUnlock()
+	id, ok := rs.stackMeta2ID[strings.ToLower(metaID)]
 	return id, ok
 }
 
 // SetMeta2PreviewID sets the cloud preview ID of a stack given its metadata ID.
 func (rs *CloudRunState) SetMeta2PreviewID(metaID string, previewID string) {
-	if rs.StackMeta2PreviewIDs == nil {
-		rs.StackMeta2PreviewIDs = make(map[string]string)
+	rs.Lock()
+	defer rs.Unlock()
+	if rs.stackMeta2PreviewIDs == nil {
+		rs.stackMeta2PreviewIDs = make(map[string]string)
 	}
-	rs.StackMeta2PreviewIDs[strings.ToLower(metaID)] = previewID
+	rs.stackMeta2PreviewIDs[strings.ToLower(metaID)] = previewID
 }
 
 // CloudPreviewID returns the cloud preview ID of a stack given its metadata ID.
-func (rs CloudRunState) CloudPreviewID(metaID string) (string, bool) {
-	id, ok := rs.StackMeta2PreviewIDs[strings.ToLower(metaID)]
+func (rs *CloudRunState) CloudPreviewID(metaID string) (string, bool) {
+	rs.RLock()
+	defer rs.RUnlock()
+	id, ok := rs.stackMeta2PreviewIDs[strings.ToLower(metaID)]
 	return id, ok
 }
 
 // SetMeta2DriftUUID sets the cloud drift UUID of a stack given its metadata ID.
 func (rs *CloudRunState) SetMeta2DriftUUID(metaID string, driftUUID resources.UUID) {
-	if rs.StackMeta2DriftUUIDs == nil {
-		rs.StackMeta2DriftUUIDs = make(map[string]resources.UUID)
+	rs.Lock()
+	defer rs.Unlock()
+	if rs.stackMeta2DriftUUIDs == nil {
+		rs.stackMeta2DriftUUIDs = make(map[string]resources.UUID)
 	}
-	rs.StackMeta2DriftUUIDs[strings.ToLower(metaID)] = driftUUID
+	rs.stackMeta2DriftUUIDs[strings.ToLower(metaID)] = driftUUID
 }
 
 // CloudDriftUUID returns the cloud drift UUID of a stack given its metadata ID.
-func (rs CloudRunState) CloudDriftUUID(metaID string) (resources.UUID, bool) {
-	id, ok := rs.StackMeta2DriftUUIDs[strings.ToLower(metaID)]
+func (rs *CloudRunState) CloudDriftUUID(metaID string) (resources.UUID, bool) {
+	rs.RLock()
+	defer rs.RUnlock()
+	id, ok := rs.stackMeta2DriftUUIDs[strings.ToLower(metaID)]
 	return id, ok
 }
 
